273c460e2253f720c34f34518ec907d3d7174287
Jack Swallow <ninjaadvance@gmail.com>
2014-02-11 17:07:46 +0700

    Add new method 'enterScene' to game_scene

--- scene/game_scene.lua
+++ scene/game_scene.lua
@@ -1,14 +1,19 @@
 storyboard = storyboard or require("storyboard")
 
 show_ground_block = show_ground_block or require("scene.show_ground_block")
+create_ground_image_sheet = create_ground_image_sheet or require("scene.create_ground_image_sheet")
+set_current_view_group = set_current_view_group or require("set_current_view_group")
+
 local scene = storyboard.newScene("game_scene")
 
-
-
 function scene:createScene(event)
-	show_ground_block.evaluate()
+	create_ground_image_sheet.evaluate()
 end
 
+function scene:enterScene(event)
+	set_current_view_group.evaluate(self.view)
+	show_ground_block.evaluate()
+end
 scene:addEventListener( "createScene", scene )
 
 return scene
--- spec/scene/game_scene_spec.lua
+++ spec/scene/game_scene_spec.lua
@@ -1,9 +1,12 @@
 describe("scene.game_scene", function ( ... )
 	local game_scene
 
-	local scene = { addEventListener = function ( ... )
-		-- body
-	end}
+	local scene = { 
+		addEventListener = function ( ... )
+			-- body
+		end,
+		view = {}
+	}
 
 	setup(function ( ... )
 		storyboard = {
@@ -15,12 +18,28 @@ describe("scene.game_scene", function ( ... )
 		show_ground_block = {}
 		stub(show_ground_block, "evaluate")
 
+		create_ground_image_sheet = {}
+		stub(create_ground_image_sheet, "evaluate")
+
+		set_current_view_group = {}
+		stub(set_current_view_group, "evaluate")
+
 		game_scene = require("scene.game_scene")
 	end)
 
-	it("Evalaute create_ground_sprites in createScene")
+	it("Evalaute create_ground_image_sheet in createScene", function ( ... )
+		-- when
+		game_scene:createScene(event)
+		-- then
+		assert.stub(create_ground_image_sheet.evaluate).was_called()
+	end)
 
-	it("Evaluate set_current_view with self.view in enterScene")
+	it("Evaluate set_current_view with self.view in enterScene", function ( ... )
+		-- when
+		game_scene:enterScene()
+		-- then
+		assert.stub(set_current_view_group.evaluate).was_called_with(scene.view)
+	end)
 
 	it("Evaluate show_ground_block in enterScene", function ( ... )
 		-- when
