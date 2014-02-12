e93a2688cef90d9f2bbd4b085a07638a9a87d0dc
Jack Swallow <ninjaadvance@gmail.com>
2014-02-12 13:53:43 +0700

    Add config file ground_block_sprite_config

---
+++ config/ground_block_sprite_config.lua
@@ -0,0 +1,9 @@
+ground_block_sprite_config = {
+	path = "img/sprite/ground.jpg",
+	options = {width = 125, height = 125, numFrames = 1},
+	sequenceData = {
+		{name="normal_with_grass", start=1, count=1}
+	}
+}
+
+return ground_block_sprite_config
