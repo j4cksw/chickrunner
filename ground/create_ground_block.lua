1438915d67bc544277bc50d4e3661db440d21d35
Jack Swallow <ninjaadvance@gmail.com>
2014-02-13 15:29:34 +0700

    Move create_ground_block to ground

---
+++ ground/create_ground_block.lua
@@ -0,0 +1,18 @@
+create_ground_block = {}
+
+insert_to_current_view_group = insert_to_current_view_group or require("scene.insert_to_current_view_group")
+get_ground_block_image_sheet = get_ground_block_image_sheet or require("scene.get_ground_block_image_sheet")
+ground_block_sprite_config = ground_block_sprite_config or require("config.ground_block_sprite_config")
+set_ground_block_position = set_ground_block_position or require("ground.set_ground_block_position")
+push_to_ground_block_queue = push_to_ground_block_queue or require("ground.push_to_ground_block_queue")
+
+function create_ground_block.evaluate()
+	local ground_block_image_sheet = get_ground_block_image_sheet.evaluate()
+	local ground_block_image = display.newSprite(ground_block_image_sheet, ground_block_sprite_config.sequenceData)
+
+	insert_to_current_view_group.evaluate(ground_block_image)
+	set_ground_block_position.evaluate(ground_block_image)
+	push_to_ground_block_queue.evaluate(ground_block_image)
+end
+
+return create_ground_block
---
+++ spec/ground/create_ground_block_spec.lua
@@ -0,0 +1,73 @@
+describe("create_ground_block", function ( ... )
+	local create_ground_block
+
+	local ground_block_image = {}
+	local ground_block_image_sheet = {}
+
+	setup(function ( ... )
+
+		get_ground_block_image_sheet = {
+			evaluate = function ( ... )
+				return ground_block_image_sheet
+			end
+		}
+		spy.on(get_ground_block_image_sheet, "evaluate")
+
+		display = { newSprite = function ( ... )
+			return ground_block_image
+		end}
+		spy.on(display, "newSprite")
+
+		set_ground_block_position = {}
+		stub(set_ground_block_position, "evaluate")
+
+		insert_to_current_view_group = {}
+		stub(insert_to_current_view_group, "evaluate")
+
+		push_to_ground_block_queue = {}
+		stub(push_to_ground_block_queue, "evaluate")
+
+		ground_block_sprite_config = {
+			sequenceData = {
+				{name="normal_with_grass", start=1, count=1}
+			}
+		}
+
+		create_ground_block = require("ground.create_ground_block")
+	end)
+
+	it("Evaluate get_ground_block_image_sheet", function ( ... )
+		-- when
+		create_ground_block.evaluate()
+		-- then
+		assert.stub(get_ground_block_image_sheet.evaluate).was_called()
+	end)
+
+	it("Create ground block image", function ( ... )
+		-- when
+		create_ground_block.evaluate()
+		-- then
+		assert.spy(display.newSprite).was_called_with(ground_block_image_sheet, ground_block_sprite_config.sequenceData)
+	end)
+
+	it("Evaluate set_ground_block_position", function ( ... )
+		-- when
+		create_ground_block.evaluate()
+		-- then
+		assert.stub(set_ground_block_position.evaluate).was_called_with(ground_block_image)
+	end)
+
+	it("Evalaute insert to current view group", function ( ... )
+		-- when
+		create_ground_block.evaluate()
+		-- then
+		assert.stub(insert_to_current_view_group.evaluate).was_called_with(ground_block_image)
+	end)
+
+	it("Evaluate push_to_ground_block_queue", function ( ... )
+		-- when
+		create_ground_block.evaluate()
+		-- then
+		assert.stub(push_to_ground_block_queue.evaluate).was_called_with(ground_block_image)
+	end)
+end)
