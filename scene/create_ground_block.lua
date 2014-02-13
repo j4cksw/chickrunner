ff9692a117b8d647844d32c8d780842a663c8573
Jack Swallow <ninjaadvance@gmail.com>
2014-02-13 12:16:33 +0700

    Add set_ground_block_position_spec

---
+++ scene/create_ground_block.lua
@@ -0,0 +1,14 @@
+create_ground_block = {}
+
+insert_to_current_view_group = insert_to_current_view_group or require("scene.insert_to_current_view_group")
+get_ground_block_image_sheet = get_ground_block_image_sheet or require("scene.get_ground_block_image_sheet")
+ground_block_sprite_config = ground_block_sprite_config or require("config.ground_block_sprite_config")
+
+function create_ground_block.evaluate()
+	local ground_block_image_sheet = get_ground_block_image_sheet.evaluate()
+	local ground_block_image = display.newSprite(ground_block_image_sheet, ground_block_sprite_config.sequenceData)
+
+	insert_to_current_view_group.evaluate(ground_block_image)
+end
+
+return create_ground_block
---
+++ spec/ground/set_ground_block_position_spec.lua
@@ -0,0 +1,11 @@
+describe("set_ground_block_position", function ( ... )
+	local set_ground_block_position
+
+	setup(function ( ... )
+		set_ground_block_position = require("ground.set_ground_block_position")
+	end)
+
+	it("Set y position on given block from ground_config")
+
+	it("Set x position of given block by calcualting position of last block in ground_blocks")
+end)
