6954b430abfb53f0efbe42290cd80e90aa2875ef
Jack Swallow <ninjaadvance@gmail.com>
2014-02-13 16:38:30 +0700

    Add evaluating check_first_ground_block in module move_ground

--- ground/move_ground.lua
+++ ground/move_ground.lua
@@ -2,6 +2,7 @@ move_ground = {}
 
 ground_config = ground_config or require("config.ground_config")
 ground_block_queue = ground_block_queue or require("ground.ground_block_queue")
+check_first_ground_block = check_first_ground_block or require("ground.check_first_ground_block")
 
 function move_ground.evaluate()
 	local move_speed = ground_config.speed
@@ -11,6 +12,8 @@ function move_ground.evaluate()
 		current_position = block.x
 		block.x = current_position - move_speed
 	end
+
+	check_first_ground_block.evaluate()
 end
 
 return move_ground
--- spec/ground/move_ground_spec.lua
+++ spec/ground/move_ground_spec.lua
@@ -2,12 +2,16 @@ describe("move_ground", function ( ... )
 	local move_ground
 
 	setup(function ( ... )
+		ground_config = { speed=5 }
+
+		check_first_ground_block = {}
+		stub(check_first_ground_block, "evaluate")
+
 		move_ground = require("ground.move_ground")
 	end)
 
 	it("Move every block in ground_block_queue to the left by speed parameter", function ( ... )
 		-- given
-		ground_config = { speed=5 }
 		ground_block_queue = {
 			{x=0}
 		}
@@ -19,7 +23,6 @@ describe("move_ground", function ( ... )
 
 	it("Move every block in ground_block_queue to the left by speed parameter", function ( ... )
 		-- given
-		ground_config = { speed=5 }
 		ground_block_queue = {
 			{x=0}, {x=10}
 		}
@@ -27,5 +30,12 @@ describe("move_ground", function ( ... )
 		move_ground.evaluate()
 		-- then
 		assert.are.equal(ground_block_queue[2].x, 5)
+	end)
+
+	it("Evalaute checkIsFirstBlockOutOfScreen", function ( ... )
+		-- when
+		move_ground.evaluate()
+		-- then
+		assert.stub(check_first_ground_block.evaluate).was_called()
 	end)
 end)
