22a81d8c0fd754b906ff461e4e1c63032907b7c5
Jack Swallow <ninjaadvance@gmail.com>
2014-02-13 16:24:28 +0700

    Improve move_ground to move all blocks in queue

--- ground/move_ground.lua
+++ ground/move_ground.lua
@@ -5,8 +5,12 @@ ground_block_queue = ground_block_queue or require("ground.ground_block_queue")
 
 function move_ground.evaluate()
 	local move_speed = ground_config.speed
-	local current_position = ground_block_queue[1].x
-	ground_block_queue[1].x = current_position + move_speed
+	local current_position
+
+	for key, block in pairs(ground_block_queue) do
+		current_position = block.x
+		block.x = current_position + move_speed
+	end
 end
 
 return move_ground
--- spec/ground/move_ground_spec.lua
+++ spec/ground/move_ground_spec.lua
@@ -16,4 +16,16 @@ describe("move_ground", function ( ... )
 		-- then
 		assert.are.equal(ground_block_queue[1].x, 5)
 	end)
+
+	it("Move every block in ground_block_queue to the left by speed parameter", function ( ... )
+		-- given
+		ground_config = { speed=5 }
+		ground_block_queue = {
+			{x=0}, {x=10}
+		}
+		-- when
+		move_ground.evaluate()
+		-- then
+		assert.are.equal(ground_block_queue[2].x, 15)
+	end)
 end)
