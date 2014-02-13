95c49fa72e3d9e8a649195add3257c4e77292554
Jack Swallow <ninjaadvance@gmail.com>
2014-02-13 17:13:56 +0700

    Implemented module check_first_ground_block

---
+++ ground/check_first_ground_block.lua
@@ -0,0 +1,16 @@
+check_first_ground_block = {}
+
+ground_block_queue = ground_block_queue or require("ground.ground_block_queue")
+create_ground_block = create_ground_block or require("ground.create_ground_block")
+
+function check_first_ground_block.evaluate()
+	local first_ground_block = ground_block_queue[1]
+
+	if first_ground_block.x <= -(first_ground_block.contentWidth/2) then 
+		table.remove(ground_block_queue, 1)
+		display.remove( first_ground_block )
+		create_ground_block.evaluate()
+	end
+end
+
+return check_first_ground_block
---
+++ spec/ground/check_first_ground_block_spec.lua
@@ -0,0 +1,44 @@
+describe("check_first_ground_block", function ( ... )
+	local check_first_ground_block
+
+	local first_block = {
+		x = -10,
+		contentWidth = 20
+	}
+
+	setup(function ( ... )
+
+		display = {}
+		stub(display, "remove")
+
+		create_ground_block = {}
+		stub(create_ground_block, "evaluate")
+
+		check_first_ground_block = require("ground.check_first_ground_block")
+	end)
+
+	before_each(function ( ... )
+		ground_block_queue = { first_block }
+	end)
+
+	it("If first ground block out of screen then remove from queue", function ( ... )
+		-- when
+		check_first_ground_block.evaluate()
+		-- then
+		assert.are.equal(#ground_block_queue, 0)
+	end)
+
+	it("If first ground block out of screen then remove from display", function ( ... )
+		-- when
+		check_first_ground_block.evaluate()
+		-- then
+		assert.stub(display.remove).was_called_with(first_block)
+	end)
+
+	it("If first ground block out of screen then evaluate create_ground_block", function ( ... )
+		-- when
+		check_first_ground_block.evaluate()
+		-- then
+		assert.stub(create_ground_block.evaluate).was_called()
+	end)
+end)
