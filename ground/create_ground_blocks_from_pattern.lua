6e9234ad91ab6ac22594bb0a46f93fc59e007125
Jack Swallow <ninjaadvance@gmail.com>
2014-02-17 22:39:55 +0700

    Remove debug print in create_ground_block

--- ground/create_ground_blocks_from_pattern.lua
+++ ground/create_ground_blocks_from_pattern.lua
@@ -5,7 +5,6 @@ create_ground_block = create_ground_block or require("ground.create_ground_block
 
 function create_ground_blocks_from_pattern.evaluate()
 	for index, value in pairs(ground_pattern[1]) do
-		print(value)
 		create_ground_block.evaluate(index, value)
 	end
 end
