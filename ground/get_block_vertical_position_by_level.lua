adcd8386c3bc2262df6f9fdaf01638d6d64e107c
Jack Swallow <ninjaadvance@gmail.com>
2014-02-17 22:20:35 +0700

    Remove unneccessary condition in module get_block_vertical_by_level

--- ground/get_block_vertical_position_by_level.lua
+++ ground/get_block_vertical_position_by_level.lua
@@ -4,12 +4,8 @@ ground_config = ground_config or require("config.ground_config")
 
 function get_block_vertical_position_by_level.evaluate(ground_block, level)
 	local vertical_start = ground_config.vertical_start
-
-	if level == 1 then
-		return vertical_start
-	else
-		return vertical_start + (ground_block.contentHeight*(level-1))
-	end
+	local content_height = ground_block.contentHeight
+	return vertical_start + (content_height*(level-1))
 end
 
 return get_block_vertical_position_by_level
