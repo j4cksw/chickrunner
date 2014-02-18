01b2ce8238789791c8ca9b283eeb656229fc6dd0
Jack Swallow <ninjaadvance@gmail.com>
2014-02-18 17:04:54 +0700

    Add setSequence and play to create_explosion_sprite

--- explosion/create_explosion_sprite.lua
+++ explosion/create_explosion_sprite.lua
@@ -1,10 +1,16 @@
 create_explosion_sprite = {}
 
 explosion_sprite_config = explosion_sprite_config or require("config.explosion_sprite_config")
+insert_to_current_view_group = insert_to_current_view_group or require("scene.insert_to_current_view_group")
 
 function create_explosion_sprite.evaluate(x, y)
   local explosion_sprite = display.newSprite(explosion_image_sheet, 
     explosion_sprite_config.sequenceData)
+    
+  insert_to_current_view_group.evaluate(explosion_sprite)
+  
+  explosion_sprite:setSequence("normal")
+  explosion_sprite:play()
 end
 
 return create_explosion_sprite
--- spec/explosion/create_explosion_sprite_spec.lua
+++ spec/explosion/create_explosion_sprite_spec.lua
@@ -16,6 +16,12 @@ describe("create_explosion_sprite", function()
     }
     spy.on(display, "newSprite")
     
+    insert_to_current_view_group = {}
+    stub(insert_to_current_view_group, "evaluate")
+    
+    stub(explosion_sprite, "setSequence")
+    stub(explosion_sprite, "play")
+    
     create_explosion_sprite = require("explosion.create_explosion_sprite")
   end)
   
@@ -28,8 +34,27 @@ describe("create_explosion_sprite", function()
       explosion_sprite_config.sequenceData)
   end)
   
-  it("Set a given position")
+  it("Insert to current view group", function()
+    -- when
+    create_explosion_sprite.evaluate(x,y)
+    -- then
+    assert.stub(insert_to_current_view_group.evaluate).was_called_with(explosion_sprite)
+  end)
   
-  it("Insert to current view group")
+  it("Set normal sequence", function()
+    -- when
+    create_explosion_sprite.evaluate(x, y)
+    -- then
+    assert.stub(explosion_sprite.setSequence).was_called_with(explosion_sprite, "normal")
+  end)
+  
+  it("Play the sprite", function() 
+    -- when
+    create_explosion_sprite.evaluate(x, y)
+    -- then
+    assert.stub(explosion_sprite.play).was_called_with(explosion_sprite)
+  end)
+  
+  it("Set a given position")
   
 end)
