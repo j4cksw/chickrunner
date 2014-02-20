2df430eb2f019b900242c0544272ad7e021eb1af
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 14:26:22 +0700

    Use for in module move_explosion

--- explosion/move_explosion.lua
+++ explosion/move_explosion.lua
@@ -3,7 +3,9 @@ move_explosion = {}
 explosion_queue = explosion_queue or require("explosion.explosion_queue")
 
 function move_explosion.evaluate(move_speed)
-    explosion_queue[1].x = explosion_queue[1].x - move_speed
+  for index, value in pairs(explosion_queue) do
+    explosion_queue[index].x = explosion_queue[index].x - move_speed
+  end
 end
 
 return move_explosion
--- spec/explosion/move_explosion_spec.lua
+++ spec/explosion/move_explosion_spec.lua
@@ -15,4 +15,17 @@ describe("move_explosion", function()
     -- then
     assert.are.equal(explosion_1.x, 80)
   end)
+  
+  it("Minus x position of every explosion sprite by a given speed", function()
+    -- given
+    local move_speed = 20
+    local explosion_1 = { x = 100 }
+    local explosion_2 = { x = 120 }
+    explosion_queue = { explosion_1, explosion_2 }
+    -- when
+    move_explosion.evaluate(move_speed)
+    -- then
+    assert.are.equal(explosion_1.x, 80)
+    assert.are.equal(explosion_2.x, 100)
+  end)
 end)
