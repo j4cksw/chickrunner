02faa219e8509a53170920e1f8fd0fc4c86d51a8
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 12:03:07 +0700

    Replace calcualting y position in module 'create_chasing_explosion' to evaluating module 'get_ground_vertical_position'

--- scene/create_chasing_explosion.lua
+++ scene/create_chasing_explosion.lua
@@ -1,14 +1,14 @@
 create_chasing_explosion = {}
 
 create_explosion_sprite = create_explosion_sprite or require("explosion.create_explosion_sprite")
-ground_config = ground_config or require("config.ground_config")
 game_scene_config = game_scene_config or require("config.game_scene_config")
+get_ground_vertical_position = get_ground_vertical_position or require("ground.get_ground_vertical_position")
 
 function create_chasing_explosion.evaluate()
   local explosion_sprite = create_explosion_sprite.evaluate()
   
   explosion_sprite.x = game_scene_config.chasing_explosion_start
-  explosion_sprite.y = ground_config.vertical_start - (explosion_sprite.contentHeight/2) - 64
+  explosion_sprite.y = get_ground_vertical_position.evaluate(explosion_sprite)
 end
 
 return create_chasing_explosion
--- spec/scene/create_chasing_explosion_spec.lua
+++ spec/scene/create_chasing_explosion_spec.lua
@@ -15,6 +15,13 @@ describe("create_chasing_explosion", function()
       end
     }
     spy.on(create_explosion_sprite, "evaluate")
+    
+    get_ground_vertical_position = {
+      evaluate=function()
+        return 1000
+      end
+    }
+    spy.on(get_ground_vertical_position, "evaluate")
   
     create_chasing_explosion = require("scene.create_chasing_explosion")
   end)
@@ -33,10 +40,17 @@ describe("create_chasing_explosion", function()
     assert.are.equal(explosion_sprite.x, 192)
   end)
   
+  it("Evalaute get_ground_vertical_position", function()
+    -- when
+    create_chasing_explosion.evaluate()
+    -- then
+    assert.stub(get_ground_vertical_position.evaluate).was_called_with(explosion_sprite)
+  end)
+  
   it("Set y to ground_config.vertical_start - explosion_sprite.contentHeight/2", function()
     -- when
     create_chasing_explosion.evaluate()
     -- then
-    assert.are.equal(explosion_sprite.y, 786)
+    assert.are.equal(explosion_sprite.y, 1000)
   end)
 end)
