5c133e1b99830ac458ba8432abc13c8d8f60110e
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 18:36:27 +0700

    Remove explosion from queue in explosion_sprite_event_listener

--- explosion/explosion_sprite_event_listener.lua
+++ explosion/explosion_sprite_event_listener.lua
@@ -1,9 +1,12 @@
 explosion_sprite_event_listener = {}
 
+explosion_queue = explosion_queue or require("explosion.explosion_queue")
+
 function explosion_sprite_event_listener.evaluate(event)
   if event.phase == "ended" then
+    table.remove(explosion_queue, 1)
     display.remove(event.target)
-    print("Removed explosion sprite")
+    --print("Removed explosion sprite")
   end
 end
 
--- spec/explosion/explosion_sprite_event_listener_spec.lua
+++ spec/explosion/explosion_sprite_event_listener_spec.lua
@@ -1,6 +1,12 @@
 describe("explosion_sprite_event_listener", function()
   local explosion_sprite_event_listener
   
+  local target = {}
+  local event = { 
+    phase = "ended",
+    target=target
+  }
+    
   setup(function()
   
     display = {}
@@ -10,16 +16,20 @@ describe("explosion_sprite_event_listener", function()
   end)
   
   it("When animate ended .Evaluate display.remove with first explosion sprite in queue", function()
-    -- given
-    local target = {}
-    local event = { 
-      phase = "ended",
-      target=target}
     -- when
     explosion_sprite_event_listener.evaluate(event)
     -- then
     assert.stub(display.remove).was_called_with(target)
   end)
   
-  it("Remove first element in explosion_queue")
+  it("Remove first element in explosion_queue", function()
+    -- given
+    local explosion_1 = {}
+    local explosion_2 = {}
+    explosion_queue = { explosion_1, explosion_2}
+    -- when
+    explosion_sprite_event_listener.evaluate(event)
+    -- then
+    assert.are_not.equal(explosion_1, explosion_queue[1])
+  end)
 end)
