faa8f9ea05911b1491aae1a3dd125968a448e32a
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 16:30:08 +0700

    Config explosion

--- config/explosion_sprite_config.lua
+++ config/explosion_sprite_config.lua
@@ -2,7 +2,7 @@ explosion_sprite_config = {
   path="img/sprite/explosion.png",
   options = {width = 320, height = 384, numFrames = 15},
   sequenceData = {
-    {name="normal", start=1, count=15, time=1000, loopCount=0}
+    {name="normal", start=1, count=15, time=500, loopCount=0}
   }
 }
 return explosion_sprite_config
--- config/game_scene_config.lua
+++ config/game_scene_config.lua
@@ -2,6 +2,6 @@ game_scene_config = {
   chasing_explosion_start=96,
   chick_start=192,
   game_speed=20,
-  explosion_delay=100
+  explosion_delay=200
 }
 return game_scene_config
