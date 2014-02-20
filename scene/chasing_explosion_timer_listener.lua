7c61cbb67a35d251def5fc13b3766f96adf12ba4
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 15:42:32 +0700

    Evaluate start_chasing_explosion_timer in module chasing_explosion_timer_listener

--- scene/chasing_explosion_timer_listener.lua
+++ scene/chasing_explosion_timer_listener.lua
@@ -1,9 +1,11 @@
 chasing_explosion_timer_listener = {}
 
 create_chasing_explosion = create_chasing_explosion or require("scene.create_chasing_explosion")
+start_chasing_explosion_timer = start_chasing_explosion_timer or require("scene.start_chasing_explosion_timer")
 
 function chasing_explosion_timer_listener.evaluate()
   create_chasing_explosion.evaluate()
+  start_chasing_explosion_timer.evaluate()
 end
 
 return chasing_explosion_timer_listener
--- spec/scene/chasing_explosion_timer_listener_spec.lua
+++ spec/scene/chasing_explosion_timer_listener_spec.lua
@@ -4,6 +4,9 @@ describe("chasing_explosion_timer_listener", function()
   setup(function()
     create_chasing_explosion = {}
     stub(create_chasing_explosion, "evaluate")
+    
+    start_chasing_explosion_timer = {}
+    stub(start_chasing_explosion_timer, "evaluate")
   
     chasing_explosion_timer_listener = require("scene.chasing_explosion_timer_listener")
   end)
@@ -15,5 +18,10 @@ describe("chasing_explosion_timer_listener", function()
     assert.stub(create_chasing_explosion.evaluate).was_called()
   end)
   
-  it("Evalaute start_chasing_explosion_timer")
+  it("Evalaute start_chasing_explosion_timer", function()
+    -- when
+    chasing_explosion_timer_listener.evaluate()
+    -- then
+    assert.stub(start_chasing_explosion_timer.evaluate).was_called()
+  end)
 end)
