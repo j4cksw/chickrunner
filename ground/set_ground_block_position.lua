448922d659e27c567bbc667507a381cbdb473043
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 18:28:36 +0700

    Comment out debug print

--- ground/set_ground_block_position.lua
+++ ground/set_ground_block_position.lua
@@ -6,8 +6,8 @@ get_next_block_horizontal_position = get_next_block_horizontal_position or requi
 function set_ground_block_position.evaluate(ground_block, level)
 	ground_block.y = get_block_vertical_position_by_level.evaluate(ground_block, level)
 	ground_block.x = get_next_block_horizontal_position.evaluate(ground_block)
-	print("contentWidth="..ground_block.contentWidth)
-	print("New block x="..ground_block.x.."y="..ground_block.y)
+	--print("contentWidth="..ground_block.contentWidth)
+	--print("New block x="..ground_block.x.."y="..ground_block.y)
 end
 
 return set_ground_block_position
