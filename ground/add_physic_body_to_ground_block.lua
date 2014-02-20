bbd1c4ad4f1bb2297768a870efa75ecdaec84934
Jack Swallow <ninjaadvance@gmail.com>
2014-02-20 19:05:09 +0700

    Add module add_physic_body_to_ground_block

---
+++ ground/add_physic_body_to_ground_block.lua
@@ -0,0 +1,14 @@
+add_physics_body_to_ground_block = {}
+
+physics = physics or require("physics")
+ground_block_sprite_config = ground_block_sprite_config or require("config.ground_block_sprite_config")
+
+function add_physics_body_to_ground_block.evaluate(ground_block)
+  physics.addBody(ground,
+    "static",
+    {
+      shape = ground_block_sprite_config.shape
+    })
+end
+
+return add_physics_body_to_ground_block
---
+++ spec/ground/add_physic_body_to_ground_block_spec.lua
@@ -0,0 +1,25 @@
+describe("add_physic_body_to_ground_block", function()
+  local add_physic_body_to_ground_block
+  
+  setup(function()
+    physics = {}
+    stub(physics, "addBody")
+    
+    add_physic_body_to_ground_block = require("ground.add_physic_body_to_ground_block")
+  end)
+  
+  it("evaluate physics.addBody with proper arguments", function()
+    -- given
+    local ground_block = {}
+    ground_block_sprite_config = {
+      shape = {}
+    }
+    -- when
+    add_physic_body_to_ground_block.evaluate(ground_block)
+    -- then flore1,"static", {bounce=0.0, shape = floreShape}
+    assert.stub(physics.addBody)
+      .was_called_with(ground_bloc,
+       "static",
+       {shape=ground_block_sprite_config.shape})
+  end)
+end)
