ce806b715572c30f29288b0cde6eb30be403014b
Jack Swallow <ninjaadvance@gmail.com>
2014-02-24 16:39:37 +0700

    Add module set_box_position

--- spec/scene/game/set_box_position_spec.lua
+++ spec/scene/game/set_box_position_spec.lua
@@ -3,10 +3,18 @@ describe("set_box_position", function()
   
   local box_sprite = {}
   
+  local evaluated_y = 1200
+  
   setup(function()
     
     game_scene_config = {
       box_horizontal_start = 1000
+    }
+    
+    get_ground_vertical_position = {
+      evaluate = function()
+        return evaluated_y
+      end
     }
     
     set_box_position = require("scene.game.set_box_position")
@@ -19,5 +27,10 @@ describe("set_box_position", function()
     assert.are.equal(box_sprite.x, game_scene_config.box_horizontal_start)
   end)
   
-  it("Set y position from evaluating get_ground_vertical_position")
+  it("Set y position from evaluating get_ground_vertical_position", function()
+    -- when
+    set_box_position.evaluate(box_sprite)
+    -- then
+    assert.are.equal(box_sprite.y, evaluated_y)
+  end)
 end)
