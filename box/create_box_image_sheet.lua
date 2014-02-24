aac376ad614b59a9e36e330c5ba0a7e58fa637fb
Jack Swallow <ninjaadvance@gmail.com>
2014-02-24 14:31:55 +0700

    Set box imagesheet to global variable 'box_image_sheet'

--- box/create_box_image_sheet.lua
+++ box/create_box_image_sheet.lua
@@ -6,7 +6,9 @@ box_sprite_config = box_sprite_config or require("config.box_sprite_config")
 function create_box_image_sheet.evaluate()
   local path = box_sprite_config.path
   local options = box_sprite_config.options
-  graphics.newImageSheet(path, options)
+  local result = graphics.newImageSheet(path, options)
+  
+  box_image_sheet = result
 end
 
 return create_box_image_sheet
