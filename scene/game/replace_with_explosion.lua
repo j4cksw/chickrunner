54650604d1c63c9061d7847502ec4b1abf9de3aa
Jack Swallow <ninjaadvance@gmail.com>
2014-02-25 16:37:41 +0700

    Add module replace_with_explosion

---
+++ scene/game/replace_with_explosion.lua
@@ -0,0 +1,16 @@
+replace_with_explosion = {}
+
+create_explosion_sprite = create_explosion_sprite or require("explosion.create_explosion_sprite")
+get_ground_vertical_position = get_ground_vertical_position or require("ground.get_ground_vertical_explosion")
+
+function replace_with_explosion.evaluate(target)
+  local box_x = target.x
+  local explosion_sprite = create_explosion_sprite.evaluate()
+  
+  explosion_sprite.x = box_x
+  explosion_sprite.y = get_ground_vertical_position.evaluate(explosion_sprite)
+  
+  display.remove(target)
+end
+
+return replace_with_explosion
---
+++ spec/scene/game/replace_with_explosion_spec.lua
@@ -0,0 +1,56 @@
+describe("replace_with_explosion", function()
+  local replace_with_explosion
+  
+  local target = { x=100}
+  
+  local explosion_sprite = {}
+  
+  setup(function()
+    
+    display = {}
+    stub(display, "remove")
+    
+    create_explosion_sprite = {
+      evaluate = function()
+        return explosion_sprite
+      end
+    }
+    spy.on(create_explosion_sprite, "evaluate")
+    
+    get_groud_vertical_position = {
+      evaluate = function()
+        return 1000
+      end
+    }
+    
+    replace_with_explosion = require("scene.game.replace_with_explosion")
+  end)
+  
+  it("Remove target sprite", function()
+    -- when
+    replace_with_explosion.evaluate(target)
+    -- then
+    assert.stub(display.remove).was_called_with(target)
+  end)
+  
+  it("Create explosion sprite", function()
+    -- when
+    replace_with_explosion.evaluate(target)
+    -- then
+    assert.stub(create_explosion_sprite.evaluate).was_called()
+  end)
+  
+  it("Set x position to be same as box last position", function()
+    -- when
+    replace_with_explosion.evaluate(target)
+    -- then
+    assert.are.equal(explosion_sprite.x, target.x)
+  end)
+  
+  it("Set y position as a result of get_ground_vertical_position", function()
+    -- when
+    replace_with_explosion.evaluate(target)
+    -- then
+    assert.are.equal(explosion_sprite.y, 1000)
+  end)
+end)
