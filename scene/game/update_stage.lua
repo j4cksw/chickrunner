16d65b64ea85502df104a9de7e7f56faa30cd289
Jack Swallow <ninjaadvance@gmail.com>
2014-02-25 14:42:59 +0700

    Move evaluating move_explosion from game_scene to update_stage

--- scene/game/update_stage.lua
+++ scene/game/update_stage.lua
@@ -1,9 +1,12 @@
 update_stage = {}
 
+game_scene_config = game_scene_config or require("config.game_scene_config")
 move_ground = move_ground or require("ground.move_ground")
+move_explosion = move_explosion or require("explosion.move_explosion")
 
 function update_stage.evaluate()
   move_ground.evaluate()
+  move_explosion.evaluate(game_scene_config.game_speed)
 end
 
 return update_stage
--- spec/scene/game/update_stage_spec.lua
+++ spec/scene/game/update_stage_spec.lua
@@ -10,6 +10,10 @@ describe("update_stage", function()
     
     move_box = {}
     stub(move_box, "evaluate") --(box_queue[1], -game_scene_config.game_speed)
+    
+    game_scene_config = {
+      game_speed = 9
+    }
   
     update_stage = require("scene.game.update_stage")
   end)
@@ -21,7 +25,12 @@ describe("update_stage", function()
     assert.stub(move_ground.evaluate).was_called()
   end)
   
-  it("Evalaute move_explosion")
+  it("Evalaute move_explosion", function()
+    -- when
+    update_stage.evaluate()
+    -- then
+    assert.stub(move_explosion.evaluate).was_called_with(game_scene_config.game_speed)
+  end)
   
   it("Evaluate move_box")
 end)
