44b755d6603258ce0d11d54c6bb0665b2f99982b
Jack Swallow <ninjaadvance@gmail.com>
2014-02-25 15:15:09 +0700

    Set output of timer.performWithDelay to explosion_timer

--- scene/start_chasing_explosion_timer.lua
+++ scene/start_chasing_explosion_timer.lua
@@ -2,9 +2,10 @@ start_chasing_explosion_timer = {}
 
 game_scene_config = game_scene_config or require("config.game_scene_config")
 chasing_explosion_timer_listener = chasing_explosion_timer_listener or require("scene.chasing_explosion_timer_listener")
+explosion_timer = explosion_timer or require("explosion.explosion_timer")
 
 function start_chasing_explosion_timer.evaluate()
-  timer.performWithDelay(game_scene_config.explosion_delay,
+  explosion_timer = timer.performWithDelay(game_scene_config.explosion_delay,
    chasing_explosion_timer_listener.evaluate)
 end
 
--- spec/scene/start_chasing_explosion_timer_spec.lua
+++ spec/scene/start_chasing_explosion_timer_spec.lua
@@ -1,13 +1,17 @@
 describe("start_chasing_explosion_timer", function()
   local start_chasing_explosion_timer
   
+  local timer_id = {}
+  
   setup(function()
     chasing_explosion_timer_listener = {
       evaluate = function() end
     }
     
     timer = {
-      performWithDelay = function()end
+      performWithDelay = function()
+        return timer_id
+      end
     }
     spy.on(timer, "performWithDelay")
     
@@ -23,4 +27,14 @@ describe("start_chasing_explosion_timer", function()
     assert.stub(timer.performWithDelay).was_called_with(100,
      chasing_explosion_timer_listener.evaluate)
   end)
+  
+  it("Set timer id to global explosion_timer", function()
+    -- give
+    explosion_timer = nil
+    -- when
+    start_chasing_explosion_timer.evaluate()
+    -- then
+    assert.are.equal(explosion_timer, timer_id)
+  end)
+  
 end)
