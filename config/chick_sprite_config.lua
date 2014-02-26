c9557490821b431fd657720be4f63e976e6b027a
Jack Swallow <ninjaadvance@gmail.com>
2014-02-26 15:45:05 +0700

    Fixed chick physics body

--- config/chick_sprite_config.lua
+++ config/chick_sprite_config.lua
@@ -9,16 +9,14 @@ chick_sprite_config = {
     {name="burn", start=1, count=8, time=100}
   },
   physics_options = {
-    {
       density=0.0,
       bounce=0.0,
       shape = {
-          60, 38,
+          60, -38,
           60, 75,
           -40, 75,
           -40, -38
       }
-    }
   }
 }
 return chick_sprite_config
