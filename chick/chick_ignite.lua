da856d6f1595f8c3c8892ec8c4aa247e119104d7
Jack Swallow <ninjaadvance@gmail.com>
2014-02-26 16:16:33 +0700

    Play chick animate after set sequence in chick_ignite

--- chick/chick_ignite.lua
+++ chick/chick_ignite.lua
@@ -2,6 +2,7 @@ chick_ignite = {}
 
 function chick_ignite.evaluate()
   chick:setSequence("ignite")
+  chick:play()
 end
 
 return chick_ignite
--- spec/chick/chick_ignite_spec.lua
+++ spec/chick/chick_ignite_spec.lua
@@ -4,7 +4,7 @@ describe("chick_ignite", function()
   setup(function()
     chick = {}
     stub(chick, 'setSequence')
-  
+    stub(chick, "play")
     chick_ignite = require("chick.chick_ignite")
   end)
   
@@ -15,7 +15,12 @@ describe("chick_ignite", function()
     assert.stub(chick.setSequence).was_called_with(chick, 'ignite')
   end)
   
-  it("Play chick sprite")
+  it("Play chick sprite", function()
+    -- when
+    chick_ignite.evaluate()
+    -- then
+    assert.stub(chick.play).was_called_with(chick)
+  end)
   
   it("Set chick_ignite_sprite_listener to sprite event listener")
 end)
