38baef0c98858c65ad149f9c9e03384c163aba47
Jack Swallow <ninjaadvance@gmail.com>
2014-02-28 08:32:22 +0700

    Add module chick_bounce_collision_listener

---
+++ scene/game/chick_bounce_collision_listener.lua
@@ -0,0 +1,11 @@
+chick_bounce_collision_listener = {}
+
+function chick_bounce_collision_listener.evaluate(event)
+  if event.phase ~= "began" and event.other.type ~= "ground" then
+    return
+  end 
+  
+  chick:removeEventListener("collision", chick_bounce_collision_listener.evaluate)
+end
+
+return chick_bounce_collision_listener
---
+++ spec/scene/game/chick_bounce_collision_listener_spec.lua
@@ -0,0 +1,28 @@
+describe("chick_bounce_collison_listener", function()
+  local chick_bounce_collision_listener
+  
+  local event
+  setup(function()
+  
+    event = {
+      phase = "began",
+      other = {
+        type = "ground"
+      }
+    }
+    
+    chick = {}
+    stub(chick, "removeEventListener")
+
+    chick_bounce_collision_listener = require("scene.game.chick_bounce_collision_listener")
+  end)
+  
+  it("When collision to the ground, Remove collision event from chick", function()
+    -- when
+    chick_bounce_collision_listener.evaluate(event)
+    -- then
+    assert.stub(chick.removeEventListener).was_called_with(chick, "collision", chick_bounce_collision_listener.evaluate)
+  end)
+  it("When collision to the ground, Set chick sequence to 'dead'")
+  it("When collision to the ground, Play the sequence ")
+end)
