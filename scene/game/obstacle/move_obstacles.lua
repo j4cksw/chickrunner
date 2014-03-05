58f4460b664f5993948c2be3000b59480b64368d
Jack Swallow <ninjaadvance@gmail.com>
2014-03-05 14:32:26 +0700

    Add for loop in move_obstacles

--- scene/game/obstacle/move_obstacles.lua
+++ scene/game/obstacle/move_obstacles.lua
@@ -4,7 +4,9 @@ move_obstacle = move_obstacle or require("scene.game.obstacle.move_obstacle")
 obstacle_queue = obstacle_queue or require("scene.game.obstacle.obstacle_queue")
 
 function move_obstacles.evaluate()
-  move_obstacle.evaluate(obstacle_queue[1])
+  for key, value in pairs(obstacle_queue) do
+    move_obstacle.evaluate(value)
+  end
 end
 
 return move_obstacles
--- spec/scene/game/obstacle/move_obstacle_spec.lua
+++ spec/scene/game/obstacle/move_obstacle_spec.lua
@@ -1,12 +1,18 @@
 describe("move_obstacles", function()
   local move_obstacles
   
+  local obstacle_1 = {}
+  local obstacle_2 = {}
+  local obstacle_3 = {}
+  
   setup(function()
     move_obstacle = {}
     stub(move_obstacle, "evaluate")
     
     obstacle_queue = {
-      {}
+      obstacle_1,
+      obstacle_2,
+      obstacle_3
     }
     
     move_obstacles = require("scene.game.obstacle.move_obstacles")
