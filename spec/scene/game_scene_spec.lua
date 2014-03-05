24c2c031c505cb3380e6f3916b6973083af08203
Jack Swallow <ninjaadvance@gmail.com>
2014-03-05 11:42:57 +0700

    Remove evalaute generatE_box from game_scene

--- spec/scene/game_scene_spec.lua
+++ spec/scene/game_scene_spec.lua
@@ -47,9 +47,6 @@ describe("scene.game_scene", function ( ... )
 
 		set_current_view_group = {}
 		stub(set_current_view_group, "evaluate")
-		
-		generate_box = {}
-		stub(generate_box, "evaluate")
 
 		Runtime = {}
 		stub(Runtime, "addEventListener")
@@ -143,13 +140,6 @@ describe("scene.game_scene", function ( ... )
 	 game_scene:enterScene(event)
 	 -- then
 	 assert.stub(start_chasing_explosion_timer.evaluate).was_called()
-	end)
-	
-	it("Evalaute generate_box in enterScene event", function()
-	 -- when
-	 game_scene:enterScene(event)
-	 -- then
-	 assert.stub(generate_box.evaluate).was_called()
 	end)
 	
 	it("Evaluate create_explosion_core in enterScene", function()
