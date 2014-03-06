50f5c09e3b7da2534f4db19987fe9427613a9d8a
Jack Swallow <ninjaadvance@gmail.com>
2014-03-06 16:37:49 +0700

    Update generate_obstacle

--- scene/game/obstacle/generate_obstacle.lua
+++ scene/game/obstacle/generate_obstacle.lua
@@ -1,5 +1,6 @@
 generate_obstacle = {}
 generate_box = generate_box or require("scene.game.generate_box")
+generate_obstacle_space = generate_obstacle_space or require("scene.game.obstacle.generate_obstacle_space") 
 obstacle_pattern = obstacle_pattern or require("config.obstacle_pattern")
 obstacle_queue = obstacle_queue or require("scene.game.obstacle.obstacle_queue")
 set_obstacle_element_position = set_obstacle_element_position or require("scene.game.obstacle.set_obstacle_element_position")
@@ -14,7 +15,11 @@ function generate_obstacle.evaluate()
     obstacle_row = {}
     table.insert(obstacle, obstacle_row)
     for element_index, pattern_element in pairs(pattern_row) do      
-      obstacle_element = generate_box.evaluate()
+      if pattern_element == "box" then
+        obstacle_element = generate_box.evaluate()
+      elseif pattern_element == "space" then
+        obstacle_element = generate_obstacle_space.evaluate()
+      end
       set_obstacle_element_position.evaluate(row_index, element_index, obstacle_element)
       table.insert(obstacle_row, obstacle_element)
     end
--- spec/scene/game/obstacle/generate_obstacle_spec.lua
+++ spec/scene/game/obstacle/generate_obstacle_spec.lua
@@ -1,15 +1,23 @@
 describe("generate_obstacle", function()
   local generate_obstacle
   
-  local fake_box = {}
+  local sample_obstacle = {}
   
   setup(function()
     generate_box = {
       evaluate = function()
-        return fake_box
+        return sample_obstacle
       end
     }
     spy.on(generate_box, "evaluate")
+    
+    
+    generate_obstacle_space = {
+      evaluate = function()
+        return sample_obstacle
+      end
+    }
+    spy.on(generate_obstacle_space, "evaluate")
     
     set_obstacle_element_position = {}
     stub(set_obstacle_element_position, "evaluate")
@@ -25,7 +33,7 @@ describe("generate_obstacle", function()
     generate_obstacle = require("scene.game.obstacle.generate_obstacle")
   end)
   
-  it("Evaluate generate box", function()
+  it("Evaluate generate_box when box available in pattern", function()
     -- when
     generate_obstacle.evaluate()
     -- then
@@ -36,6 +44,21 @@ describe("generate_obstacle", function()
     -- when
     generate_obstacle.evaluate()
     -- then
-    assert.stub(set_obstacle_element_position.evaluate).was_called_with(1, 1, fake_box)
+    assert.stub(set_obstacle_element_position.evaluate).was_called_with(1, 1, sample_obstacle)
   end)
+  
+  it("Evaluate generate_space when space available in pattern", function()
+    -- given
+    obstacle_pattern = {
+      {
+        { "space" }
+      }
+    }
+    -- when
+    generate_obstacle.evaluate()
+    -- then
+    assert.stub(generate_obstacle_space.evaluate).was_called()
+  end)
+  
+  
 end)
