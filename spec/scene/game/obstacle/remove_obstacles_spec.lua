f21cf99c874d77075f477152eaa562667555d465
Jack Swallow <ninjaadvance@gmail.com>
2014-03-07 12:48:42 +0700

    Add module remove_obstacles

---
+++ spec/scene/game/obstacle/remove_obstacles_spec.lua
@@ -0,0 +1,9 @@
+describe("remove_obstacle", function()
+  local remove_obstacle
+  
+  setup(function()
+    remove_obstacle = require("scene.game.obstacle.remove_obstacles")
+  end)
+  
+  it("Evaluate display.remove with every elements in obstacle queue")
+end)
