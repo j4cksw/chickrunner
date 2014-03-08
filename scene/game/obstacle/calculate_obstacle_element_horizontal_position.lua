351091d0116111960a748fd58fd90d928896120f
Jack Swallow <ninjaadvance@gmail.com>
2014-03-08 21:53:13 +0700

    Fixed fail tests in calculate_obstacle_element_horizontal_position

--- scene/game/obstacle/calculate_obstacle_element_horizontal_position.lua
+++ scene/game/obstacle/calculate_obstacle_element_horizontal_position.lua
@@ -2,15 +2,17 @@ calculate_obstacle_element_horizontal_position = {}
 
 obstacle_queue = obstacle_queue or require("scene.game.obstacle.obstacle_queue")
 game_scene_config = game_scene_config or require("config.game_scene_config")
-get_last_obstacle_element_position = get_last_obstacle_element_position or require("scene.game.obstacle.get_last_obstacle_element_position")
+get_last_obstacle_horizontal_position = get_last_obstacle_horizontal_position or require("scene.game.obstacle.get_last_obstacle_horizontal_position")
 
 function calculate_obstacle_element_horizontal_position.evaluate(element_index, element_sprite)
+  local obstacle_group_start
   if #obstacle_queue == 0 then
-    return game_scene_config.obstacle_horizontal_start  
+    obstacle_group_start = game_scene_config.obstacle_horizontal_start  
   else
-    local last_obstacle_element_position = get_last_obstacle_element_position.evaluate()
-    return last_obstacle_element_position+(element_sprite.contentWidth/2)
+    obstacle_group_start = get_last_obstacle_horizontal_position.evaluate()
   end
+  
+  return obstacle_group_start+(element_sprite.contentWidth/2)+(element_sprite.contentWidth*(element_index-1))
 end
 
 return calculate_obstacle_element_horizontal_position
--- spec/scene/game/obstacle/calculate_obstacle_element_horizontal_position_spec.lua
+++ spec/scene/game/obstacle/calculate_obstacle_element_horizontal_position_spec.lua
@@ -12,43 +12,24 @@ describe("calculate_obstacle_element_horizontal_position", function()
       obstacle_horizontal_start = 720
     }
     
-    get_last_obstacle_element_position = {
+    get_last_obstacle_horizontal_position = {
       evaluate = function()
         return last_obstacle_position
       end
     }
-    spy.on(get_last_obstacle_element_position, "evaluate")
+    spy.on(get_last_obstacle_horizontal_position, "evaluate")
+    
     
     calculate_obstacle_element_horizontal_position = require("scene.game.obstacle.calculate_obstacle_element_horizontal_position")
   end)
   
-  it("Evalaute get_last_obstacle_element_position", function()
+  it("Evalaute get_last_obstacle_horizontal_position", function()
     -- given
     obstacle_queue = { "x" }
     -- when
     calculate_obstacle_element_horizontal_position.evaluate(1, sample_obstacle_element)
     -- then
-    assert.stub(get_last_obstacle_element_position.evaluate).was_called()
-  end)
-  
-  
-  it("Return calcualted x position value from given index and element", function()
-    -- given
-    obstacle_queue = { "x" }
-    -- when
-    local result = calculate_obstacle_element_horizontal_position.evaluate(1, sample_obstacle_element)
-    -- then
-    assert.are.equal(result, 770)
-  end)
-  
-  it("Return calcualted x position value from given index and element", function()
-    -- given
-    obstacle_queue = { "x" }
-    sample_obstacle_element.contentWidth = 128
-    -- when
-    local result = calculate_obstacle_element_horizontal_position.evaluate(1, sample_obstacle_element)
-    -- then
-    assert.are.equal(result, 784)
+    assert.stub(get_last_obstacle_horizontal_position.evaluate).was_called()
   end)
   
   it("Return obstacle starting point from config if no obstacle available", function()
@@ -57,6 +38,6 @@ describe("calculate_obstacle_element_horizontal_position", function()
     -- when
     local result = calculate_obstacle_element_horizontal_position.evaluate(1, sample_obstacle_element)
     -- then
-    assert.are.equal(result, game_scene_config.obstacle_horizontal_start)
+    assert.are.equal(result, 770)
   end)
 end)
