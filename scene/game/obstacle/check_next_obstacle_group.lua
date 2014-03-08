48402febe0edae0516f57b76f99ca2e20fb50414
Jack Swallow <ninjaadvance@gmail.com>
2014-03-08 22:25:34 +0700

    Implemented case evaluating generate_obstacle condition in module check_next_obstacle

--- scene/game/obstacle/check_next_obstacle_group.lua
+++ scene/game/obstacle/check_next_obstacle_group.lua
@@ -1,9 +1,13 @@
 check_next_obstacle_group = {}
 
 get_last_obstacle_horizontal_position = get_last_obstacle_horizontal_position or require("scene.game.obstacle.get_last_obstacle_horizontal_position")
+generate_obstacle = generate_obstacle or require("scene.game.obstacle.generate_obstacle")
 
 function check_next_obstacle_group.evaluate()
-  get_last_obstacle_horizontal_position.evaluate()
+  local last_element_position = get_last_obstacle_horizontal_position.evaluate()
+  if last_element_position <= display.viewableContentWidth then
+    generate_obstacle.evaluate()
+  end
 end
 
 return check_next_obstacle_group
--- spec/scene/game/check_next_obstacle_group_spec.lua
+++ spec/scene/game/check_next_obstacle_group_spec.lua
@@ -1,12 +1,23 @@
 describe("check_next_obstacle_group", function()
   local check_next_obstacle_group
   
+  local last_obstacle_position = 1000
+  
   setup(function()
     get_last_obstacle_horizontal_position = {
-      evaluate = function()end
+      evaluate = function()
+        return last_obstacle_position
+      end
     }
     spy.on(get_last_obstacle_horizontal_position, "evaluate")
-  
+    
+    display = {
+      viewableContentWidth = 1000
+    }
+    
+    generate_obstacle = {}
+    stub(generate_obstacle, "evaluate")
+    
     check_next_obstacle_group = require("scene.game.obstacle.check_next_obstacle_group")
   end)
   
@@ -17,5 +28,10 @@ describe("check_next_obstacle_group", function()
     assert.stub(get_last_obstacle_horizontal_position.evaluate).was_called()
   end)
   
-  it("If result lower than screenWidth then evaluate generate_obstacle")
+  it("If result lower or equal screenWidth then evaluate generate_obstacle", function()
+    -- when
+    check_next_obstacle_group.evaluate()
+    -- then
+    assert.stub(generate_obstacle.evaluate).was_called()
+  end)
 end)
