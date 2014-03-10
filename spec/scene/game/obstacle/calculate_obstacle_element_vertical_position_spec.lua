ed958ff0eb0145605acb436c03d4f2fcaa7a9e32
Jack Swallow <ninjaadvance@gmail.com>
2014-03-10 15:01:55 +0700

    Refactor to parameterized test in calculate_obstacle_element_vertical_position_spec

--- spec/scene/game/obstacle/calculate_obstacle_element_vertical_position_spec.lua
+++ spec/scene/game/obstacle/calculate_obstacle_element_vertical_position_spec.lua
@@ -18,12 +18,6 @@ describe("calculate_obstacle_element_vertical_position", function()
     calculate_obstacle_element_vertical_position = require("scene.game.obstacle.calculate_obstalce_element_vertical_position")
   end)
   
-  local test_data = {
-    { row_index=1, expected_result=1000 },
-    { row_index=2, expected_result=1200 },
-    { row_index=3, expected_result=1400 }
-  }
-  
   it("Evaluate get_ground_vertical_position", function()
     -- when
     calculate_obstacle_element_vertical_position.evaluate(1, sample_obstacle_element)
@@ -31,13 +25,20 @@ describe("calculate_obstacle_element_vertical_position", function()
     assert.stub(get_ground_vertical_position.evaluate).was_called_with(sample_obstacle_element)
   end)
   
-  it("Return expected result when given", function()
-    -- given
-    local row_index = 1
-    local expected_result = 1000
-    -- when
-    local result = calculate_obstacle_element_vertical_position.evaluate(row_index, sample_obstacle_element)
-    -- then
-    assert.are.equal(result, expected_result)
-  end)
+  local test_data = {
+    { row_index=1, expected_result=1000 },
+    { row_index=2, expected_result=800 }
+  }
+  
+  for index, data in pairs(test_data) do
+    it("Return data "..data.expected_result.." when given row_index "..data.row_index, function()
+      -- given
+      local row_index = data.row_index
+      local expected_result = data.expected_result
+      -- when
+      local result = calculate_obstacle_element_vertical_position.evaluate(row_index, sample_obstacle_element)
+      -- then
+      assert.are.equal(result, expected_result)
+    end)
+  end
 end)
