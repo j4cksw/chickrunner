3b5f1bff0ef5f5f256eace884e4843a5a0ed1d6e
Jack Swallow <ninjaadvance@gmail.com>
2014-03-12 19:46:25 +0700

    Set update score to score_text

--- scene/game/score/increase_score.lua
+++ scene/game/score/increase_score.lua
@@ -2,6 +2,7 @@ increase_score = {}
 
 function increase_score.evaluate()
   current_score = current_score+1
+  score_text.text = current_score.." m"
 end
 
 return increase_score
--- spec/scene/game/score/increase_score_spec.lua
+++ spec/scene/game/score/increase_score_spec.lua
@@ -2,6 +2,8 @@ describe("increase_score", function()
   local increase_score
   
   setup(function()
+    score_text = {}
+  
     increase_score = require("scene.game.score.increase_score")
   end)
   
@@ -14,5 +16,12 @@ describe("increase_score", function()
     assert.are.equal(current_score, 1)
   end)
   
-  it("Update score text.")
+  it("Update score text.", function()
+    -- given
+    current_score = 0
+    -- when
+    increase_score.evaluate()
+    -- then
+    assert.are.equal(score_text.text, "1 m")
+  end)
 end)
