d2ec5d111dd638b00d2ffa704808a2eeeb4a2a03
Jack Swallow <ninjaadvance@gmail.com>
2014-03-12 14:03:38 +0700

    Refactor destroy_all_obstacle to user _.each

--- scene/game/obstacle/destroy_all_obstacles.lua
+++ scene/game/obstacle/destroy_all_obstacles.lua
@@ -2,9 +2,12 @@ destroy_all_obstacles = {}
 
 obstacle_queue = obstacle_queue or require("scene.game.obstacle.obstacle_queue")
 destroy_obstacle_group = destroy_obstacle_group or require("scene.game.obstacle.destroy_obstacle_group")
+_ = require("lib.underscore")
 
 function destroy_all_obstacles.evaluate()
-  destroy_obstacle_group.evaluate(obstacle_queue[1])
+  _.each(obstacle_queue, function(obstacle_group)
+    destroy_obstacle_group.evaluate(obstacle_group)
+  end)
 end
 
 return destroy_all_obstacles
