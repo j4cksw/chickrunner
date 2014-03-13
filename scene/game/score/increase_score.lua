bda9daea6f13f185e92bd23256fbbb6304c04f22
Jack Swallow <ninjaadvance@gmail.com>
2014-03-13 12:25:10 +0700

    Fixed score text

--- scene/game/score/increase_score.lua
+++ scene/game/score/increase_score.lua
@@ -2,7 +2,7 @@ increase_score = {}
 
 function increase_score.evaluate()
   current_score = current_score+1
-  score_text.text = current_score.." m"
+  score_text.text = current_score.."m"
 end
 
 return increase_score
