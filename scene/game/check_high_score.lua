0d23daac11cc71cb2a93e67b3a6cdb6a48569ace
Jack Swallow <ninjaadvance@gmail.com>
2014-03-18 21:16:39 +0700

    Add evalaute save_game_stat_file to check_high_score

--- scene/game/check_high_score.lua
+++ scene/game/check_high_score.lua
@@ -1,8 +1,11 @@
 check_high_score = {}
 
+save_game_stat_file = save_game_stat_file or require("utils.save_game_stat_file")
+
 function check_high_score.evaluate()
   if current_score > game_stat.high_score then
     game_stat.high_score = current_score
+    save_game_stat_file.evaluate()
   end
 end
 
