6f5e26bc8220f9a27af18328e4023d2e4d418d21
Jack Swallow <ninjaadvance@gmail.com>
2014-03-18 20:40:11 +0700

    Open file from path

--- scene/game/load_game_stat_file.lua
+++ scene/game/load_game_stat_file.lua
@@ -2,6 +2,7 @@ load_game_stat_file = {}
 
 function load_game_stat_file.evaluate()
   local path = system.pathForFile("game_stat", system.DocumentsDirectory)
+  local file = io.open(path, "r")
 end
 
 return load_game_stat_file
--- spec/scene/game/load_game_stat_file_spec.lua
+++ spec/scene/game/load_game_stat_file_spec.lua
@@ -1,14 +1,31 @@
 describe("load_game_stat_file", function()
   local load_game_stat_file
   
+  local fake_game_stat_path
+  local fake_game_stat_file
+  
+  local expectedResult = {}
+
+  json = {
+    decode = function ( ... )
+      return expectedResult
+    end
+  }
+  
   setup(function()
     system = {
       pathForFile = function ( ... )
-        return fakeHighscorePath
+        return fake_game_stat_path
       end,
       DocumentsDirectory = "fakeDocumentDir"
     }
     spy.on(system, "pathForFile")
+    
+    io.open = function ( ... )
+      return fake_game_stat_file
+    end
+    spy.on(io, "open")
+    stub(io, "close")
     
     load_game_stat_file = require("scene.game.load_game_stat_file")
   end)
@@ -18,5 +35,12 @@ describe("load_game_stat_file", function()
     load_game_stat_file.evaluate()
     -- then
     assert.stub(system.pathForFile).was_called_with("game_stat", system.DocumentsDirectory)
+  end)
+  
+  it("Open file", function ( ... )
+    -- when
+    load_game_stat_file.evaluate()
+    -- then
+    assert.stub(io.open).was_called_with(fake_game_stat_path, "r")
   end)
 end)
