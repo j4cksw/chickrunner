c71e0483770e80e19ee4c3be5a758601405ac94e
Jack Swallow <ninjaadvance@gmail.com>
2014-03-19 15:12:28 +0700

    Add chick idle state

--- config/chick_sprite_config.lua
+++ config/chick_sprite_config.lua
@@ -1,14 +1,15 @@
 chick_sprite_config = {
   path = "img/sprite/chick.png",
   options = {
-    width = 256, height = 256, numFrames = 39
+    width = 256, height = 256, numFrames = 48
   },
   sequenceData = {
     {name="run", start=31, count=8, time=350},
     {name="jump", start=28, count=4, time=100},
     {name="ignite", start=1, count=3, time=300, loopCount=1},
     {name="burn", start=4, count=5, time=300},
-    {name="dead", start=9, count=19, time=1500, loopCount=1}
+    {name="dead", start=9, count=19, time=1500, loopCount=1},
+    {name="idle", start=40, count=9, time=1000}
   },
   physics_options = {
       friction=0.0,
