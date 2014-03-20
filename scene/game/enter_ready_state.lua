a522ee6a02a980d762f62952c681f30a7a8a3f04
Jack Swallow <ninjaadvance@gmail.com>
2014-03-20 19:32:55 +0700

    Evalaute generate_obstacles_from_config in enter_ready_State

--- scene/game/enter_ready_state.lua
+++ scene/game/enter_ready_state.lua
@@ -2,11 +2,13 @@ enter_ready_state = {}
 
 initialize_ground = initialize_ground or require("ground.initialize_ground")
 initialize_chick = initialize_chick or require("scene.initialize_chick")
+generate_obstacles_from_config = generate_obstacles_from_config or require("scene.game.obstacle.generate_obstacles_from_config")
 explode_before_start = explode_before_start or require("scene.game.explode_before_start")
 
 function enter_ready_state.evaluate()
   initialize_ground.evaluate()
   initialize_chick.evaluate()
+  generate_obstacles_from_config.evaluate()
   Runtime:addEventListener( "tap", explode_before_start.evaluate)
 end
 
