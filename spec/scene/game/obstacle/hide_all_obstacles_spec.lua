2994935f9d918441c5d2c4718d6305f68925ee8b
Jack Swallow <ninjaadvance@gmail.com>
2014-03-20 19:42:57 +0700

    Add module hide_all_obstacle

---
+++ spec/scene/game/obstacle/hide_all_obstacles_spec.lua
@@ -0,0 +1,9 @@
+describe("hide_all_obstacles", function()
+  local hide_all_obstacles
+  
+  setup(function()
+    hide_all_obstacles = require("scene.game.obstacle.hide_all_obstacles")
+  end)
+  
+  it("Set isVisible property of every obstacles in queue to false")
+end)
