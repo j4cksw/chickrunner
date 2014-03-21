167759963dabf72da3e2a9e9b5dc12a8cd6305e2
Jack Swallow <ninjaadvance@gmail.com>
2014-03-21 13:32:11 +0700

    Create first line text of ready_state_ui

--- scene/game/create_ready_state_ui.lua
+++ scene/game/create_ready_state_ui.lua
@@ -1,9 +1,12 @@
 create_ready_state_ui = {}
 
 create_tap_to_start_button = create_tap_to_start_button or require("scene.game.create_tap_to_start_button")
+get_fontname_by_platform = get_fontname_by_platform or require("utils.get_fontname_by_platform")
+
 
 function create_ready_state_ui.evaluate()
   create_tap_to_start_button.evaluate()
+  display.newText("TO", display.contentCenterX, tap_to_start_button.y+20, get_fontname_by_platform.evaluate(), 32)
 end
 
 return create_ready_state_ui
--- spec/scene/game/create_ready_state_ui_spec.lua
+++ spec/scene/game/create_ready_state_ui_spec.lua
@@ -1,9 +1,36 @@
 describe("create_ready_state_ui", function()
   local create_ready_state_ui
   
+  tap_to_start_button = {
+    y = 500
+  }
+  
+  local fontname = "f"
+  
   setup(function()
-    create_tap_to_start_button = {}
+    create_tap_to_start_button = {
+      evalaute = function()
+        return tap_to_start_button 
+      end
+    }
     stub(create_tap_to_start_button, "evaluate")
+    
+    get_fontname_by_platform = {}
+    stub(get_fontname_by_platform, "evaluate")
+    
+    display = {
+      contentCenterX=600,
+      newText = function()
+        return fake_ending_score_text
+      end
+    }
+    spy.on(display, "newText")
+    
+    get_fontname_by_platform = {
+      evaluate = function()
+        return fontname
+      end
+    }
     
     create_ready_state_ui = require("scene.game.create_ready_state_ui")
   end)
@@ -14,5 +41,16 @@ describe("create_ready_state_ui", function()
     -- then
     assert.stub(create_tap_to_start_button.evaluate).was_called()
   end)
+  
+  it("Create 'to' text under tap button", function()
+    -- when
+    create_ready_state_ui.evaluate()
+    -- then
+    assert.stub(display.newText).was_called_with("TO", display.contentCenterX, tap_to_start_button.y+20, fontname, 32)
+  end)
+  
+  it("Create text 'start' under 'to'")
+  
+  it("Create display group")
   
 end)
