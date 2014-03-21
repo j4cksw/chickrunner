3d78bfe0bf5ed217d7ab10ab1910f2e4a26d5296
Jack Swallow <ninjaadvance@gmail.com>
2014-03-21 11:16:22 +0700

    Fixed explosion bug

--- explosion/explosion_sprite_event_listener.lua
+++ explosion/explosion_sprite_event_listener.lua
@@ -3,10 +3,9 @@ explosion_sprite_event_listener = {}
 explosion_queue = explosion_queue or require("explosion.explosion_queue")
 
 function explosion_sprite_event_listener.evaluate(event)
+  print("Remove from display")
   if event.phase == "ended" then
-    table.remove(explosion_queue, 1)
     display.remove(event.target)
-    --print("Removed explosion sprite")
   end
 end
 
