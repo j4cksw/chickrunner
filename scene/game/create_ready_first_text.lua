8205d97148ecf55f49211d1e7ae5f7fc8f12f305
Jack Swallow <ninjaadvance@gmail.com>
2014-03-23 16:00:57 +0700

    Add module create_ready_first_text

---
+++ scene/game/create_ready_first_text.lua
@@ -0,0 +1,13 @@
+create_ready_first_text = {}
+
+game_scene_config = game_scene_config or require("config.game_scene_config")
+
+function create_ready_first_text.evaluate(pivot)
+  display.newText("TO",
+       display.contentCenterX,
+       pivot.y+game_scene_config.ready_first_text_y_offset,
+       fontname, 
+       game_scene_config.ready_first_text_size)
+end
+
+return create_ready_first_text
