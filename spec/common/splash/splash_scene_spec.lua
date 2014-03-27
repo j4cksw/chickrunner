409516a0c7dcfc0b72bd5b917df6a6083e688530
Jack Swallow <ninjaadvance@gmail.com>
2014-03-27 22:36:16 +0700

    Add splash_scene

---
+++ spec/common/splash/splash_scene_spec.lua
@@ -0,0 +1,9 @@
+describe("splash_scene", function()
+  local splash_scene
+  
+  setup(function()
+    splash_scene = require("common.splash.splash_scene")
+  end)
+  
+  it("Should show a logo image")
+end)
