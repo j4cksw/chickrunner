7aadaffe2c6b7e26b7acf90a3ff74eb30990849a
Jack Swallow <ninjaadvance@gmail.com>
2014-03-31 12:25:25 +0700

    Evaluate load_global_audio at main

--- main.lua
+++ main.lua
@@ -1,5 +1,8 @@
 display.setStatusBar( display.HiddenStatusBar )
 
+load_global_audio = require("scene.load_global_audio")
+load_global_audio.evaluate()
+
 storyboard = storyboard or require("storyboard")
 --storyboard.gotoScene("scene.game.game_scene")
 --storyboard.gotoScene("scene.title.title_scene")
