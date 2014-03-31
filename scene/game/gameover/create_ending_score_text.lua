08c18eebe8e11ee437d58b76cb00fce6f3ba5ac1
Jack Swallow <ninjaadvance@gmail.com>
2014-03-31 21:04:49 +0700

    Add module create_ending_score_text

---
+++ scene/game/gameover/create_ending_score_text.lua
@@ -0,0 +1,14 @@
+create_ending_score_text = {}
+
+game_scene_config = game_scene_config or require("config.game_scene_config")
+get_fontname_by_platform = get_fontname_by_platform or require("utils.get_fontname_by_platform")
+
+function create_ending_score_text.evaluate()
+  ending_score_text = display.newText("Distance: "..current_score.."m",
+    display.contentCenterX,
+    game_scene_config.ending_score_text_y,
+    get_fontname_by_platform.evaluate(),
+    game_scene_config.ending_score_text_size)
+end
+
+return create_ending_score_text
---
+++ spec/scene/game/gameover/create_ending_score_text_spec.lua
@@ -0,0 +1,47 @@
+describe("ceate_final_score_text", function()
+  local create_ending_score_text
+
+  local fontname = ""
+
+  setup(function()
+    get_fontname_by_platform = {
+      evaluate = function()
+        return fontname
+      end
+    }
+
+    display = {
+      contentCenterX=600,
+      newText = function()
+        return fake_ending_score_text
+      end
+    }
+    spy.on(display, "newText")
+
+    game_scene_config = {
+      ending_score_text_y = 65,
+      ending_score_text_size = 125,
+      ending_high_score_text_y = 75
+    }
+
+    create_ending_score_text = require("scene.game.gameover.create_ending_score_text")
+  end)
+
+  it("When sprite animate ended show ending_score_text", function()
+    -- when
+    create_ending_score_text.evaluate()
+    -- then
+    assert.stub(display.newText).was_called_with("Distance: 100m",
+      display.contentCenterX,
+      game_scene_config.ending_score_text_y,
+      fontname,
+      game_scene_config.ending_score_text_size)
+  end)
+
+  it("When sprite animate ended set text to global ending_score_text", function()
+    -- when
+    create_ending_score_text.evaluate()
+    -- then
+    assert.are.equal(ending_score_text, fake_ending_score_text)
+  end)
+end)
