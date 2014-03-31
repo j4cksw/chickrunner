a2a7f61356c1535a48109684ae845d7bab2a6dad
Jack Swallow <ninjaadvance@gmail.com>
2014-03-31 11:49:08 +0700

    Add module load_global_audio

---
+++ spec/scene/load_global_audio_spec.lua
@@ -0,0 +1,9 @@
+describe("load_global_audio", function()
+  local load_global_audio
+  
+  setup(function()
+    load_global_audio = require("scene.load_global_audio")
+  end)
+  
+  it("should load ui button sound")
+end)
