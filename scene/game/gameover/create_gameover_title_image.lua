f984432be814aa627d5d8670181825970dd43912
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 18:21:53 +0700

    Set gameover title image y position to 215

--- scene/game/gameover/create_gameover_title_image.lua
+++ scene/game/gameover/create_gameover_title_image.lua
@@ -3,6 +3,7 @@ create_gameover_title_image = {}
 function create_gameover_title_image.evaluate()
   gameover_title_image = display.newImage("img/screen/gameover/gameover.png")
   gameover_title_image.x = display.contentCenterX
+  gameover_title_image.y = 215
 end
 
 return create_gameover_title_image
--- spec/scene/game/gameover/create_gameover_title_image_spec.lua
+++ spec/scene/game/gameover/create_gameover_title_image_spec.lua
@@ -1,8 +1,8 @@
 describe("create_gameover_title_image", function()
   local create_gameover_title_image
-  
+
   local fake_gameover_title_image = {"..."}
-  
+
   setup(function()
     display = {
       contentCenterX = 50,
@@ -11,19 +11,25 @@ describe("create_gameover_title_image", function()
       end
     }
     spy.on(display, "newImage")
-    
+
     create_gameover_title_image = require("scene.game.gameover.create_gameover_title_image")
   end)
-  
+
   it("should create gameover title image", function()
     create_gameover_title_image.evaluate()
-    
+
     assert.stub(display.newImage).was_called_with("img/screen/gameover/gameover.png")
   end)
-  
+
   it("should set x to center of screen", function()
     create_gameover_title_image.evaluate()
-    
+
     assert.are.equal(fake_gameover_title_image.x, 50)
+  end)
+
+  it("should set y to 215", function()
+    create_gameover_title_image.evaluate()
+
+    assert.are.equal(fake_gameover_title_image.y, 215)
   end)
 end)
