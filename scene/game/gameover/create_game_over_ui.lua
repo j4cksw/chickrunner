4ee37ff4c7bcac7f7285075a7e8d84347a5d1720
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 17:52:53 +0700

    Add module create_game_over_ui

--- scene/game/gameover/create_game_over_ui.lua
+++ scene/game/gameover/create_game_over_ui.lua
@@ -1,18 +1,16 @@
-chick_dead_sequence_ended_listener = {}
+create_game_over_ui = {}
 
 check_high_score = check_high_score or require("scene.game.check_high_score")
 show_ads = show_ads or require("common.ads.show_ads")
 create_ending_score_text = create_ending_score_text or require("scene.game.gameover.create_ending_score_text")
 create_ending_high_score_text = create_ending_high_score_text or require("scene.game.gameover.create_ending_high_score_text")
 
-function chick_dead_sequence_ended_listener.evaluate(event)
-  if event.phase == "ended" then
-    show_ads.evaluate()
-    score_text.isVisible = false
-    create_ending_score_text.evaluate()
-    check_high_score.evaluate()
-    create_ending_high_score_text.evaluate()
-  end
+function create_game_over_ui.evaluate()
+  show_ads.evaluate()
+  score_text.isVisible = false
+  create_ending_score_text.evaluate()
+  check_high_score.evaluate()
+  create_ending_high_score_text.evaluate()
 end
 
-return chick_dead_sequence_ended_listener
+return create_game_over_ui
--- spec/scene/game/gameover/create_game_over_ui_spec.lua
+++ spec/scene/game/gameover/create_game_over_ui_spec.lua
@@ -1,12 +1,7 @@
-describe("chick_dead_sequence_ended_listener.evaluate", function()
-  local chick_dead_sequence_ended_listener
-  
-  local event = {
-    phase = "ended"
-  }
+describe("create_game_over_ui", function()
+  local create_game_over_ui
   
   setup(function()
-    
     score_text = {}
     
     check_high_score = {}
@@ -21,40 +16,40 @@ describe("chick_dead_sequence_ended_listener.evaluate", function()
     create_ending_high_score_text = {}
     stub(create_ending_high_score_text, "evaluate")
   
-    chick_dead_sequence_ended_listener = require("scene.game.chick.chick_dead_sequence_ended_listener") 
+    create_game_over_ui = require("scene.game.gameover.create_game_over_ui") 
   end)
   
   it("should show ads when sprite animate ended", function()
     -- when
-    chick_dead_sequence_ended_listener.evaluate(event)
+    create_game_over_ui.evaluate()
     -- then
     assert.stub(show_ads.evaluate).was_called()
   end)
   
-  it("When sprite animate ended hide score_text", function()
+  it("should hide score_text", function()
     -- when
-    chick_dead_sequence_ended_listener.evaluate(event)
+    create_game_over_ui.evaluate()
     -- then
     assert.are.False(score_text.isVisible)
   end)
   
-  it("should create ending_score_text when sprite animate ended", function()
+  it("should create ending_score_text", function()
     -- when
-    chick_dead_sequence_ended_listener.evaluate(event)
+    create_game_over_ui.evaluate()
     -- then
     assert.stub(create_ending_score_text.evaluate).was_called()
   end)
   
-  it("when sprite animate ended. Evalaute check_high_score", function()
+  it("should evalaute check_high_score", function()
     -- when
-    chick_dead_sequence_ended_listener.evaluate(event)
+    create_game_over_ui.evaluate()
     -- then
     assert.stub(check_high_score.evaluate).was_called()
   end)
   
-  it("When sprite animate ended show ending_score_text", function()
+  it("should show ending_score_text", function()
     -- when
-    chick_dead_sequence_ended_listener.evaluate(event)
+    create_game_over_ui.evaluate(event)
     -- then
     assert.stub(create_ending_high_score_text.evaluate).was_called()
   end)
