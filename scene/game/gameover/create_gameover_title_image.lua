668b2b38a6af2eed407e849bc8997cdfc73db733
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 18:19:21 +0700

    Set gameover title image x to center

--- scene/game/gameover/create_gameover_title_image.lua
+++ scene/game/gameover/create_gameover_title_image.lua
@@ -1,7 +1,8 @@
 create_gameover_title_image = {}
 
 function create_gameover_title_image.evaluate()
-  display.newImage("img/screen/gameover/gameover.png")
+  gameover_title_image = display.newImage("img/screen/gameover/gameover.png")
+  gameover_title_image.x = display.contentCenterX
 end
 
 return create_gameover_title_image
--- spec/scene/game/gameover/create_gameover_title_image_spec.lua
+++ spec/scene/game/gameover/create_gameover_title_image_spec.lua
@@ -1,9 +1,16 @@
 describe("create_gameover_title_image", function()
   local create_gameover_title_image
   
+  local fake_gameover_title_image = {"..."}
+  
   setup(function()
-    display = {}
-    stub(display, "newImage")
+    display = {
+      contentCenterX = 50,
+      newImage = function()
+        return fake_gameover_title_image
+      end
+    }
+    spy.on(display, "newImage")
     
     create_gameover_title_image = require("scene.game.gameover.create_gameover_title_image")
   end)
@@ -12,5 +19,11 @@ describe("create_gameover_title_image", function()
     create_gameover_title_image.evaluate()
     
     assert.stub(display.newImage).was_called_with("img/screen/gameover/gameover.png")
+  end)
+  
+  it("should set x to center of screen", function()
+    create_gameover_title_image.evaluate()
+    
+    assert.are.equal(fake_gameover_title_image.x, 50)
   end)
 end)
