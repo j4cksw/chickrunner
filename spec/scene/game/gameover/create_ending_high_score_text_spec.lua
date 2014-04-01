b268e624c7ae58347b19b127400919ff5306108d
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 15:27:13 +0700

    Add module create_ending_high_score_text

---
+++ spec/scene/game/gameover/create_ending_high_score_text_spec.lua
@@ -0,0 +1,37 @@
+describe("create_ending_highscore_text", function()
+  local create_ending_high_score_text
+  
+  setup(function()
+    create_ending_high_score_text = require("scene.game.gameover.create_ending_high_score_text")
+  end)
+  
+  it("should evaluate text with proper parameters", function()
+    game_stat = {
+      high_score = 1000
+    }
+    
+    display = {
+      contentCenterX = 5
+    }
+    
+    game_scene_config = {
+      ending_high_score_text_format = "Best: %dm",
+      ending_high_score_text_y = 10,
+      ending_score_text_size = 32
+    }
+    
+    create_ending_high_score_text.evaluate()
+    
+    assert.stub(create_text.evaluate).was_called_with("Best: 100m",
+      display.contentCenterX,
+      game_scene_config.ending_high_score_text_y,
+      game_scene_config.ending_score_text_size)
+  end)
+
+  it("When sprite animate ended set text to global ending_score_text", function()
+    -- when
+    create_ending_high_score_text.evaluate()
+    -- then
+    assert.are.equal(ending_score_text, fake_ending_score_text)
+  end)
+end)
