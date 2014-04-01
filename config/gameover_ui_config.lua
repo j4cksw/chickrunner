cfac548663ac3719f9b5d1dac9a9b727a6dfa06d
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 20:48:35 +0700

    Add property score_panel_background_image_path to gameover_ui_config

--- config/gameover_ui_config.lua
+++ config/gameover_ui_config.lua
@@ -1,6 +1,6 @@
 gameover_ui_config = {
   gameover_title_image_path="img/screen/gameover/gameover.png",
-  gameover_title_image_y=215
+  gameover_title_image_y=215,
   score_panel_background_image_path="img/screen/gameover/bg.png",
 }
 return gameover_ui_config
