592d7bcb2c30cc762ded231e2188c1bfd5ab6eae
Jack Swallow <ninjaadvance@gmail.com>
2014-04-01 18:26:53 +0700

    Change from hardcode to config in create_gameover_title_image

--- scene/game/gameover/create_gameover_title_image.lua
+++ scene/game/gameover/create_gameover_title_image.lua
@@ -5,7 +5,7 @@ gameover_ui_config = gameover_ui_config or require("config.gameover_ui_config")
 function create_gameover_title_image.evaluate()
   gameover_title_image = display.newImage(gameover_ui_config.gameover_title_image_path)
   gameover_title_image.x = display.contentCenterX
-  gameover_title_image.y = 215
+  gameover_title_image.y = gameover_ui_config.gameover_title_image_y
 end
 
 return create_gameover_title_image
