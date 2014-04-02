bd4a166fe79385a40bba59a46d486d3463782475
Jack Swallow <ninjaadvance@gmail.com>
2014-04-02 16:21:09 +0700

    Insert gameover title image and score panel background to gameover ui group

--- scene/game/gameover/create_gameover_ui_group.lua
+++ scene/game/gameover/create_gameover_ui_group.lua
@@ -2,6 +2,8 @@ create_gameover_ui_group = {}
 
 function create_gameover_ui_group.evaluate()
   gameover_ui_group = display.newGroup()
+  gameover_ui_group:insert(gameover_title_image)
+  gameover_ui_group:insert(score_panel_background)
 end
 
 return create_gameover_ui_group
--- spec/scene/game/gameover/create_gameover_ui_group_spec.lua
+++ spec/scene/game/gameover/create_gameover_ui_group_spec.lua
@@ -10,6 +10,9 @@ describe("create_gameover_ui_group", function()
       end
     }
     spy.on(display, "newGroup")
+    
+    stub(fake_ui_group, "insert")
+    
     create_gameover_ui_group = require("scene.game.gameover.create_gameover_ui_group")
   end)
   
@@ -25,8 +28,20 @@ describe("create_gameover_ui_group", function()
     assert.are.equal(gameover_ui_group, fake_ui_group)
   end)
   
-  it("should add gameover title image to group")
+  it("should add gameover title image to group", function()
+    gameover_title_image = {"fake_gameover_title_image"}
+    
+    create_gameover_ui_group.evaluate()
+    
+    assert.stub(fake_ui_group.insert).was_called_with(fake_ui_group, gameover_title_image)
+  end)
   
-  it("should add score panel background to group")
+  it("should add score panel background to group", function()
+    score_panel_background = {"fake_score_panel_background"}
+    
+    create_gameover_ui_group.evaluate()
+    
+    assert.stub(fake_ui_group.insert).was_called_with(fake_ui_group, score_panel_background)
+  end)
   
 end)
