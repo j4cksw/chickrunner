50f69912621acc11cbad903f3c790541cbe51c58
Jack Swallow <ninjaadvance@gmail.com>
2014-04-02 22:17:25 +0700

    Add properties to gameover_ui_config

--- config/gameover_ui_config.lua
+++ config/gameover_ui_config.lua
@@ -2,6 +2,9 @@ gameover_ui_config = {
   gameover_title_image_path="img/screen/gameover/gameover.png",
   gameover_title_image_y=215,
   score_panel_background_image_path="img/screen/gameover/bg.png",
-  score_panel_background_image_y=448
+  score_panel_background_image_y=448,
+  play_again_button_image_path="img/ui/play.png",
+  play_again_button_x = display.contentCenterX,
+  play_again_button_y = 745
 }
 return gameover_ui_config
