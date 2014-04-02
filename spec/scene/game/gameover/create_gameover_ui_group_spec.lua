e0e5d17c31a35477191838a00378d8ed3e353eb7
Jack Swallow <ninjaadvance@gmail.com>
2014-04-02 16:15:43 +0700

    Set global varialble gameover_ui_group

--- spec/scene/game/gameover/create_gameover_ui_group_spec.lua
+++ spec/scene/game/gameover/create_gameover_ui_group_spec.lua
@@ -1,9 +1,15 @@
 describe("create_gameover_ui_group", function()
   local create_gameover_ui_group
   
+  local fake_ui_group = {"..."}
+  
   setup(function()
-    display = {}
-    stub(display, "newGroup")
+    display = {
+      newGroup = function()
+        return fake_ui_group
+      end
+    }
+    spy.on(display, "newGroup")
     create_gameover_ui_group = require("scene.game.gameover.create_gameover_ui_group")
   end)
   
@@ -13,7 +19,11 @@ describe("create_gameover_ui_group", function()
     assert.stub(display.newGroup).was_called()
   end)
   
-  it("should set to global variable 'gameover_ui_group'")
+  it("should set to global variable 'gameover_ui_group'", function()
+    create_gameover_ui_group.evaluate()
+    
+    assert.are.equal(gameover_ui_group, fake_ui_group)
+  end)
   
   it("should add gameover title image to group")
   
