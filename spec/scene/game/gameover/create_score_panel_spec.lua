6f2098d142c2a444736f35b2207bbbfba225dbee
Jack Swallow <ninjaadvance@gmail.com>
2014-04-03 15:08:49 +0700

    Add evaluating create_high_score_label in create_score_panel

--- spec/scene/game/gameover/create_score_panel_spec.lua
+++ spec/scene/game/gameover/create_score_panel_spec.lua
@@ -10,6 +10,9 @@ describe("create_score_panel", function()
 
       create_ending_score_text = {}
       stub(create_ending_score_text, "evaluate")
+      
+      create_ending_high_score_label = {}
+      stub(create_ending_high_score_label, "evaluate")
 
       create_ending_high_score_text = {}
       stub(create_ending_high_score_text, "evaluate")
@@ -35,7 +38,13 @@ describe("create_score_panel", function()
     assert.stub(create_ending_score_text.evaluate).was_called()
   end)
   
-  it("should show ending_highscore_text", function()
+  it("should create ending_high_score_label", function()
+    create_score_panel.evaluate()
+    
+    assert.stub(create_ending_high_score_label.evaluate).was_called()
+  end)
+  
+  it("should show ending_high_score_text", function()
     create_score_panel.evaluate()
     
     assert.stub(create_ending_high_score_text.evaluate).was_called()
