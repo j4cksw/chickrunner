db98705632ebc822fbbb02528bddd29fc0d38e39
Jack Swallow <ninjaadvance@gmail.com>
2014-04-03 12:08:22 +0700

    Add module create_ending_core_label

---
+++ scene/game/gameover/create_ending_score_label.lua
@@ -0,0 +1,7 @@
+create_ending_score_label = {}
+
+function create_ending_score_label.evaluate()
+  ending_score_label = display.newImage("img/screen/gameover/score.png")
+end
+
+return create_ending_score_label
---
+++ spec/scene/game/gameover/create_ending_score_label_spec.lua
@@ -0,0 +1,32 @@
+describe("create_ending_score_label", function()
+  local create_ending_score_label
+  
+  local fake_image = {"fake_image"}
+  
+  setup(function()
+    display = {
+      newImage = function()
+        return fake_image
+      end
+    }
+    spy.on(display, "newImage")
+    
+    create_ending_score_label = require("scene.game.gameover.create_ending_score_label")
+  end)
+  
+  it("should create ending score label image", function()
+    create_ending_score_label.evaluate()
+    
+    assert.stub(display.newImage).was_called_with("img/screen/gameover/score.png")
+  end)
+  
+  it("should set to global variable ending_score_label", function()
+    create_ending_score_label.evaluate()
+    
+    assert.are.equal(fake_image, ending_score_label)
+  end)
+  
+  it("should set x to 220")
+  
+  it("should set y to 374")
+end)
