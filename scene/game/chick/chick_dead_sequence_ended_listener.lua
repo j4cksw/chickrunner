d90c74836df7dbc6f6764c7aaf91a68492477628
Jack Swallow <ninjaadvance@gmail.com>
2014-04-05 15:48:05 +0700

    Add evaluating play gameover sound wwhen dead animation end

--- scene/game/chick/chick_dead_sequence_ended_listener.lua
+++ scene/game/chick/chick_dead_sequence_ended_listener.lua
@@ -1,10 +1,12 @@
 chick_dead_sequence_ended_listener = {}
 
 create_game_over_ui = create_game_over_ui or require("scene.game.gameover.create_game_over_ui")
+play_gameover_sound = play_gameover_sound or require("scene.game.gameover.play_gameover_sound")
 
 function chick_dead_sequence_ended_listener.evaluate(event)
   if event.phase == "ended" then
     create_game_over_ui.evaluate()
+    play_gameover_sound.evaluate()
   end
 end
 
--- spec/scene/game/chick/chick_dead_sequence_ended_listener_spec.lua
+++ spec/scene/game/chick/chick_dead_sequence_ended_listener_spec.lua
@@ -6,9 +6,12 @@ describe("chick_dead_sequence_ended_listener.evaluate", function()
   }
   
   setup(function()
-    
+
     create_game_over_ui = {}
     stub(create_game_over_ui, "evaluate")
+    
+    play_gameover_sound = {}
+    stub(play_gameover_sound, "evaluate")
   
     chick_dead_sequence_ended_listener = require("scene.game.chick.chick_dead_sequence_ended_listener") 
   end)
@@ -17,6 +20,12 @@ describe("chick_dead_sequence_ended_listener.evaluate", function()
     chick_dead_sequence_ended_listener.evaluate(event)
     
     assert.stub(create_game_over_ui.evaluate).was_called()
+  end)
+  
+  it("should play gameover sound when sprite animate ended", function()
+    chick_dead_sequence_ended_listener.evaluate(event)
+    
+    assert.stub(play_gameover_sound.evaluate).was_called()
   end)
   
 end)
