786890b2ab447ce9f4bffa32768c86db9b32afc8
Jack Swallow <ninjaadvance@gmail.com>
2014-04-08 19:17:09 +0700

    Load title bgm stream

---
+++ scene/title/load_title_audio.lua
@@ -0,0 +1,7 @@
+load_title_audio = {}
+
+function load_title_audio.evaluate()
+  audio.loadStream("audio/menu_bg.mp3")
+end
+
+return load_title_audio
--- spec/scene/title/load_title_audio_spec.lua
+++ spec/scene/title/load_title_audio_spec.lua
@@ -1,11 +1,24 @@
 describe("load_title_audio", function()
   local load_title_audio
   
+  local fake_stream = {"..."}
+  
   setup(function()
+    audio = {
+      loadStream = function()
+        return fake_stream      
+      end
+    }
+    stub(audio, "loadStream")
+    
     load_title_audio = require("scene.title.load_title_audio")
   end)
   
-  it("should load title bgm sound")
+  it("should load title bgm sound", function()
+    load_title_audio.evaluate()
+    
+    assert.stub(audio.loadStream).was_called_with("audio/menu_bg.mp3")
+  end)
   
   it("should set to global variable title_bgm")
 end)
