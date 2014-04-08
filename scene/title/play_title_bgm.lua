5134600a76523fee77f59670d1ad04e2ef905b38
Jack Swallow <ninjaadvance@gmail.com>
2014-04-08 19:29:19 +0700

    play title bgm

---
+++ scene/title/play_title_bgm.lua
@@ -0,0 +1,7 @@
+play_title_bgm = {}
+
+function play_title_bgm.evaluate()
+  audio.play(title_bgm)
+end
+
+return play_title_bgm
--- spec/scene/title/play_title_bgm_spec.lua
+++ spec/scene/title/play_title_bgm_spec.lua
@@ -2,8 +2,17 @@ describe("play_title_bgm", function()
   local play_title_bgm
   
   setup(function()
+    audio = {}
+    stub(audio, "play")
+    
+    title_bgm = {"title_bgm"}
+    
     play_title_bgm = require("scene.title.play_title_bgm")
   end)
   
-  it("should play title bgm")
+  it("should play title bgm", function()
+    play_title_bgm.evaluate()
+    
+    assert.stub(audio.play).was_called_with(title_bgm)
+  end)
 end)
