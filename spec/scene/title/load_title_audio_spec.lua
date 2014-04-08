b27cf51d505b32fbd1843fdd0e791a743938a4d3
Jack Swallow <ninjaadvance@gmail.com>
2014-04-08 19:08:48 +0700

    Add module load_title_audio

---
+++ spec/scene/title/load_title_audio_spec.lua
@@ -0,0 +1,11 @@
+describe("load_title_audio", function()
+  local load_title_audio
+  
+  setup(function()
+    load_title_audio = require("scene.title.load_title_audio")
+  end)
+  
+  it("should load title bgm sound")
+  
+  it("should set to global variable title_bgm")
+end)
