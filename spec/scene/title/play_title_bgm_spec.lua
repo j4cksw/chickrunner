b66b642717d0bf1aca9980fdd115bf852fae4deb
Jack Swallow <ninjaadvance@gmail.com>
2014-04-08 19:26:16 +0700

    Add moudle play title bgm

---
+++ spec/scene/title/play_title_bgm_spec.lua
@@ -0,0 +1,9 @@
+describe("play_title_bgm", function()
+  local play_title_bgm
+  
+  setup(function()
+    play_title_bgm = require("scene.title.play_title_bgm")
+  end)
+  
+  it("should play title bgm")
+end)
