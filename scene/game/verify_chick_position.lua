4787adab2429785c9378ad4f279c4993a712a3a9
Jack Swallow <ninjaadvance@gmail.com>
2014-04-09 14:45:52 +0700

    Add chick sequence in verify_chick_position

--- scene/game/verify_chick_position.lua
+++ scene/game/verify_chick_position.lua
@@ -3,7 +3,7 @@ verify_chick_position = {}
 game_scene_config = game_scene_config or require("config.game_scene_config")
 
 function verify_chick_position.evaluate()
-  if chick.x < game_scene_config.chick_start then
+  if chick.x < game_scene_config.chick_start and chick.sequence == "run" then
     chick:setLinearVelocity(100, 0.0)
   end
 end
--- spec/scene/game/verify_chick_position_spec.lua
+++ spec/scene/game/verify_chick_position_spec.lua
@@ -1,21 +1,24 @@
 describe("verify_chick_position", function()
   local verify_chick_position
-  
+
   setup(function()
     verify_chick_position = require("scene.game.verify_chick_position")
   end)
-  
+
   it("should move chick to start position when its x position lower than config", function()
     game_scene_config = {
       chick_start=192
     }
     chick = {
-      x = 191
+      x = 191,
+      sequence="run"
     }
     stub(chick, "setLinearVelocity")
-    
+
     verify_chick_position.evaluate()
-    
+
     assert.stub(chick.setLinearVelocity).was_called_with(chick, 100, 0.0)
   end)
+
+  it("should stop moving when chick reached the start position")
 end)
