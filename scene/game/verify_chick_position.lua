acd185d67bb7643c8a019960b6ed9cc01b8677ba
Jack Swallow <ninjaadvance@gmail.com>
2014-04-09 14:41:07 +0700

    Set linear velocity to chick when position lower than start

--- scene/game/verify_chick_position.lua
+++ scene/game/verify_chick_position.lua
@@ -1,2 +1,11 @@
 verify_chick_position = {}
+
+game_scene_config = game_scene_config or require("config.game_scene_config")
+
+function verify_chick_position.evaluate()
+  if chick.x < game_scene_config.chick_start then
+    chick:setLinearVelocity(100, 0.0)
+  end
+end
+
 return verify_chick_position
