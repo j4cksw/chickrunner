2885ce9e2da571a545ba4a8d51a6617a9437ad3a
Jack Swallow <ninjaadvance@gmail.com>
2014-04-09 15:06:17 +0700

    Fixed chick position

--- scene/game/verify_chick_position.lua
+++ scene/game/verify_chick_position.lua
@@ -5,9 +5,9 @@ game_scene_config = game_scene_config or require("config.game_scene_config")
 function verify_chick_position.evaluate()
   if chick.x < game_scene_config.chick_start and chick.sequence == "run" then
     chick:setLinearVelocity(100, 0.0)
-  end
-  if chick.x >= game_scene_config.chick_start and chick.sequence == "run" then
-    chick:setLinearVelocity(0.0, 0.0)
+    if chick.x >= game_scene_config.chick_start and chick.sequence == "run" then
+      chick:setLinearVelocity(0.0, 0.0)
+    end
   end
 end
 
--- utils/get_fontname_by_platform.lua
+++ utils/get_fontname_by_platform.lua
@@ -4,7 +4,6 @@ fontname_table = fontname_table or require("config.fontname_table")
 
 function get_fontname_by_platform.evaluate()
 	local platform_name = system.getInfo( "platformName" )
-	print(platform_name)
 	local result = fontname_table[platform_name]
 	if not result then
 		return fontname_table["Others"]
