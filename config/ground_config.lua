67706384c9dca034401a02b11499083bc759e982
Jack Swallow <ninjaadvance@gmail.com>
2014-04-09 22:14:20 +0700

    Fixed point to remove ground offset

--- config/ground_config.lua
+++ config/ground_config.lua
@@ -1,7 +1,7 @@
 ground_config = {
 	vertical_start=1036,
-	initial_block=8,
+	initial_block=10,
 	speed=20,
-	horizontal_start_position_offset=150
+	horizontal_start_position_offset=200
 }
 return ground_config
--- ground/check_first_ground_block.lua
+++ ground/check_first_ground_block.lua
@@ -7,7 +7,7 @@ increase_score = increase_score or require("scene.game.score.increase_score")
 function check_first_ground_block.evaluate()
 	local first_ground_block = ground_block_queue[1][1]
 
-	if first_ground_block.x <= -(first_ground_block.contentWidth/2) then 
+	if first_ground_block.x <= -(first_ground_block.contentWidth*2) then 
 		table.remove(ground_block_queue, 1)
 		display.remove( first_ground_block )
 		create_ground_blocks_from_pattern.evaluate()
