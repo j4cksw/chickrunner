d3cbcc02476b52905504fd95b4b3b843f2489cc1
Jack Swallow <ninjaadvance@gmail.com>
2014-04-10 11:34:13 +0700

    Add property remove_ground-block_position to ground_config

--- config/ground_config.lua
+++ config/ground_config.lua
@@ -2,6 +2,7 @@ ground_config = {
 	vertical_start=1036,
 	initial_block=10,
 	speed=20,
-	horizontal_start_position_offset=200
+	horizontal_start_position_offset=200,
+	remove_ground_block_position = -256
 }
 return ground_config
