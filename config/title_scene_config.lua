b002c19a910457318b7ade2781b72189342f8c58
Jack Swallow <ninjaadvance@gmail.com>
2014-04-14 12:11:26 +0700

    Add title_scene_config

---
+++ config/title_scene_config.lua
@@ -0,0 +1,7 @@
+title_scene_config = {
+  start_button_default_file="img/ui/start.png",
+  start_button_over_file="img/ui/start_down.png",
+  start_button_x=10,
+  start_button_y=740
+}
+return title_scene_config
