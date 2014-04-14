fe8da5cb2c749dcc4ee0334cd5c49cd26233aa11
Jack Swallow <ninjaadvance@gmail.com>
2014-04-14 12:10:19 +0700

    Change hardocde to configuration in create_Start_button

--- scene/title/create_start_button.lua
+++ scene/title/create_start_button.lua
@@ -3,17 +3,19 @@ create_start_button = {}
 widget = widget or require("widget")
 insert_to_current_view_group = insert_to_current_view_group or require("scene.insert_to_current_view_group")
 transit_to_game_scene = transit_to_game_scene or require("scene.title.transit_to_game_scene")
+play_ui_button_sound = play_ui_button_sound or require("scene.audio.play_ui_button_sound")
+title_scene_config = title_scene_config or require("config.title_scene_config")
 
 function create_start_button.evaluate()
   local start_button = widget.newButton({
     id = "start",
-    defaultFile = "img/ui/start.png",
-    overFile="img/ui/start_down.png",
+    defaultFile = title_scene_config.start_button_default_file,
+    overFile=title_scene_config.start_button_over_file,
     onRelease = transit_to_game_scene.evaluate,
     onPress = play_ui_button_sound.evaluate
   })
-  start_button.x = display.contentCenterX
-  start_button.y = 740
+  start_button.x = title_scene_config.start_button_x
+  start_button.y = title_scene_config.start_button_y
   
   insert_to_current_view_group.evaluate(start_button)
 end
--- spec/scene/title/create_start_button_spec.lua
+++ spec/scene/title/create_start_button_spec.lua
@@ -4,16 +4,23 @@ describe("create_start_button", function()
   local fake_button = {}
   
   setup(function()
+    display = {
+      contentCenterX = 10,
+    }
+    
+    title_scene_config = {
+      start_button_default_file="img/ui/start.png",
+      start_button_over_file="img/ui/start_down.png",
+      start_button_x=10,
+      start_button_y=740
+    }
+  
     widget = {
       newButton = function()
         return fake_button
       end
     }
     spy.on(widget, "newButton")
-    
-    display = {
-      contentCenterX = 10,
-    }
     
     play_ui_button_sound = {
       evaluate=function()end
