f082a7034d32c609db69acd94bb8c58c588429ce
Jack Swallow <ninjaadvance@gmail.com>
2014-04-14 12:38:58 +0700

    Add more properties to title_scene_config

--- config/title_scene_config.lua
+++ config/title_scene_config.lua
@@ -2,6 +2,9 @@ title_scene_config = {
   start_button_default_file="img/ui/start.png",
   start_button_over_file="img/ui/start_down.png",
   start_button_x=display.contentCenterX,
-  start_button_y=740
+  start_button_y=740,
+  logo_image_file="img/screen/title/logo.png",
+  logo_image_x=10,
+  logo_image_y=370
 }
 return title_scene_config
