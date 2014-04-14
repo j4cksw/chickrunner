eed7eac592060a06bfc5b561bee7c55ba2e9b7ca
Jack Swallow <ninjaadvance@gmail.com>
2014-04-14 12:31:19 +0700

    Extracted creating logo image from title_scene

--- scene/title/title_scene.lua
+++ scene/title/title_scene.lua
@@ -3,11 +3,10 @@ storyboard = storyboard or require("storyboard")
 widget = widget or require("widget")
 set_current_view_group = set_current_view_group or require("scene.set_current_view_group")
 create_background_image = create_background_image or require("scene.create_background_image")
-insert_to_current_view_group = insert_to_current_view_group or require("scene.insert_to_current_view_group")
 create_ground_image_sheet = create_ground_image_sheet or require("scene.create_ground_image_sheet")
 create_start_button = create_start_button or require("scene.title.create_start_button")
+create_logo_image = create_logo_image or require("scene.title.create_logo_image")
 initialize_ground = initialize_ground or require("ground.initialize_ground")
-show_ads = show_ads or require("common.ads.show_ads")
 load_title_audio = load_title_audio or require("scene.title.load_title_audio")
 play_title_bgm = play_title_bgm or require("scene.title.play_title_bgm")
 
@@ -19,12 +18,7 @@ function title_scene:createScene(event)
   set_current_view_group.evaluate(self.view)
   create_background_image.evaluate()
   create_start_button.evaluate()
-  
-  local logo_image = display.newImage("img/screen/title/logo.png")
-  logo_image.x = display.contentCenterX
-  logo_image.y = 370
-  insert_to_current_view_group.evaluate(logo_image)
-  
+  create_logo_image.evaluate()
   create_ground_image_sheet.evaluate()
   initialize_ground.evaluate()
 end
--- spec/scene/title/title_scene_spec.lua
+++ spec/scene/title/title_scene_spec.lua
@@ -31,16 +31,19 @@ describe("title_scene", function()
 
     create_background_image = {}
     stub(create_background_image, "evaluate")
-    
+
     create_start_button = {}
     stub(create_start_button, "evaluate")
 
+    create_logo_image = {}
+    stub(create_logo_image, "evaluate")
+
     insert_to_current_view_group = {}
     stub(insert_to_current_view_group, "evaluate")
-    
+
     load_title_audio = {}
     stub(load_title_audio, "evaluate")
-    
+
     play_title_bgm = {}
     stub(play_title_bgm, "evaluate")
 
@@ -63,47 +66,25 @@ describe("title_scene", function()
 
   it("should create start button", function()
     title_scene:createScene(event)
-    
+
     assert.stub(create_start_button.evaluate).was_called()
   end)
 
-  it("Create game title image", function()
-    -- when
+  it("should create game logo image", function()
     title_scene:createScene(event)
-    -- then
-    assert.stub(display.newImage).was_called_with("img/screen/title/logo.png")
+
+    assert.stub(create_logo_image.evaluate).was_called()
   end)
 
-  it("Set x of title image to center of screen", function()
-    -- when
-    title_scene:createScene(event)
-    -- then
-    assert.are.equal(fake_logo.x, display.contentCenterX)
-  end)
-
-  it("Set y of title image to 200", function()
-    -- when
-    title_scene:createScene(event)
-    -- then
-    assert.are.equal(fake_logo.y, 370)
-  end)
-
-  it("Insert title image to current view group", function()
-    -- when
-    title_scene:createScene(event)
-    -- then
-    assert.stub(insert_to_current_view_group.evaluate).was_called_with(fake_logo)
-  end)
-  
   it("should load audio files that use in title", function()
     title_scene:createScene(event)
-    
+
     assert.stub(load_title_audio.evaluate).was_called()
   end)
-  
+
   it("should play title bgm", function()
     title_scene:createScene(event)
-    
+
     assert.stub(play_title_bgm.evaluate).was_called()
   end)
 end)
