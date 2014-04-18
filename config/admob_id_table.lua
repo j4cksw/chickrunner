88ee4bb8cbf86cadc229f1cc87fa410471131de7
Jack Swallow <ninjaadvance@gmail.com>
2014-04-18 18:12:42 +0700

    Remove unused code

--- config/admob_id_table.lua
+++ config/admob_id_table.lua
@@ -1,6 +1,9 @@
 admob_id_table = {
   ["Android"] = "a1534511cd248dc",
-  ["iPhone OS"] = "a15350a7a1d099c"
+  ["iPhone OS"] = {
+    ["iPhone"] = "a15350a7a1d099c",
+    ["iPad"] = "a15350a829b8cdc"
+  }
 }
 
 return admob_id_table
--- spec/common/ads/get_admob_id_by_platform_spec.lua
+++ spec/common/ads/get_admob_id_by_platform_spec.lua
@@ -1,43 +1,48 @@
 describe("get_admob_id_by_platform", function()
   local get_admob_id_by_platform
-  
+
   local fake_platform_name = ""
+  local fake_model_name = ""
   
   setup(function()
     system = {
-      getInfo = function()
-        return fake_platform_name
+      getInfo = function(key)
+        if key == "platformName" then
+          return fake_platform_name
+        elseif key == "model" then
+          return fake_model_name
+        end
       end
     }
     spy.on(system, "getInfo")
-    
+
     admob_id_table = {
       ["Android"] = "a1534511cd248dc",
       ["iPhone OS"] = "a15350a7a1d099c"
     }
-    
+
     get_admob_id_by_platform = require("common.ads.get_admob_id_by_platform")
   end)
-  
+
   it("should return Android's admob id by device platform", function()
     fake_platform_name = "Android"
-    
+
     local result = get_admob_id_by_platform.evaluate()
-    
+
     assert.are.equal(result, "a1534511cd248dc")
   end)
-  
+
   it("should return iOS's admob id by device platform", function()
     fake_platform_name = "iPhone OS"
     
     local result = get_admob_id_by_platform.evaluate()
-    
+
     assert.are.equal(result, "a15350a7a1d099c")
   end)
-  
+
   it("should get device's platform name from system", function()
     get_admob_id_by_platform.evaluate()
-    
+
     assert.stub(system.getInfo).was_called_with("platformName")
   end)
 end)
