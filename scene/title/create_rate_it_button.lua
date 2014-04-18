ef1a02604645086c15129e28aaf713548aab39af
Jack Swallow <ninjaadvance@gmail.com>
2014-04-18 11:32:53 +0700

    Create a rate-it button in title_scene

--- scene/title/create_rate_it_button.lua
+++ scene/title/create_rate_it_button.lua
@@ -1,6 +1,12 @@
 create_rate_it_button = {}
 
+widget = widget or require("widget")
+
 function create_rate_it_button.evaluate()
+  widget.newButton({
+    id="rate-it",
+    defaultFile="img/ui/rate.png"
+  })
 end
 
 return create_rate_it_button
