8ef81d8e46f8d636ebdbabca53e29ec003fb2c49
Jack Swallow <ninjaadvance@gmail.com>
2014-04-18 14:04:28 +0700

    Add sound when pressing rate-it button

--- spec/scene/title/create_rate_it_button_spec.lua
+++ spec/scene/title/create_rate_it_button_spec.lua
@@ -11,8 +11,19 @@ describe("create_rate_it_button", function()
     }
     spy.on(widget, "newButton")
     
+    title_scene_config = {
+      rate_it_button_default_file="img/ui/rate.png",
+      rate_it_button_over_file="img/ui/rate_down.png",
+      rate_it_button_x=10,
+      rate_it_button_y=900
+    }
+    
     insert_to_current_view_group = {}
     stub(insert_to_current_view_group, "evaluate")
+    
+    play_ui_button_sound = {
+      evaluate = function()end
+    }
     
     create_rate_it_button = require("scene.title.create_rate_it_button")
   end)
@@ -23,19 +34,21 @@ describe("create_rate_it_button", function()
     assert.stub(widget.newButton).was_called_with({
       id = "rate-it",
       defaultFile = "img/ui/rate.png",
+      overFile= "img/ui/rate_down.png",
+      onPress = play_ui_button_sound.evaluate
     })
   end)
   
   it("should set x to 583", function()
     create_rate_it_button.evaluate()
     
-    assert.are.equal(fake_button.x, 583)
+    assert.are.equal(fake_button.x, 360)
   end)
   
   it("should set y to 1161", function()
     create_rate_it_button.evaluate()
     
-    assert.are.equal(fake_button.y, 1161)
+    assert.are.equal(fake_button.y, 900)
   end)
   
   it("should insert to current view group", function()
