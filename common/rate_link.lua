6883b0761905171202c238b3ef46f343c71806b3
Jack Swallow <ninjaadvance@gmail.com>
2014-04-18 20:02:32 +0700

    Add link to iTunes appstore

--- common/rate_link.lua
+++ common/rate_link.lua
@@ -1,8 +1,14 @@
 rate_link = {}
 
 function rate_link.open()
-  system.getInfo("platformName")
-  system.openURL("market://details?id=com.kongdev.chickrunner")
+  if system.getInfo("platformName") == "Android" then
+    system.openURL("market://details?id=com.kongdev.chickrunner")
+  elseif system.getInfo("platformName") == "iPhone OS" then
+    system.openURL("itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa"
+      .. "/wa/viewContentsUserReviews?"
+      .. "type=Purple+Software&id="
+      .. "860430668")
+  end
 end
 
 return rate_link
--- spec/common/rate_link_spec.lua
+++ spec/common/rate_link_spec.lua
@@ -1,9 +1,15 @@
 describe("rate_link", function()
   local rate_link
   
+  local fake_platform_name = ""
+  
   setup(function()
-    system = {}
-    stub(system, "getInfo")
+    system = {
+      getInfo = function()
+        return fake_platform_name
+      end
+    }
+    spy.on(system, "getInfo")
     stub(system, "openURL")
   
     rate_link = require("common.rate_link")
@@ -15,9 +21,22 @@ describe("rate_link", function()
     assert.stub(system.getInfo).was_called_with("platformName")
   end)
   
-  it("should open assigned rating link for Android", function()
+  it("should open rating link for Android", function()
+    fake_platform_name = "Android"
+    
     rate_link.open()
     
     assert.stub(system.openURL).was_called_with("market://details?id=com.kongdev.chickrunner")
+  end)
+  
+  it("should open rating link for iOS", function()
+    fake_platform_name = "iPhone OS"
+    
+    rate_link.open()
+    
+    assert.stub(system.openURL).was_called_with("itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa"
+                 .. "/wa/viewContentsUserReviews?"
+                 .. "type=Purple+Software&id="
+                 .. "860430668")
   end)
 end)
