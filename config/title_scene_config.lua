2d15894658a41ccfa07e5b5311884c49c080a569
Jack Swallow <ninjaadvance@gmail.com>
2014-04-18 14:08:35 +0700

    Add rate-it button properties to title_scene_config

--- config/title_scene_config.lua
+++ config/title_scene_config.lua
@@ -5,6 +5,10 @@ title_scene_config = {
   start_button_y=740,
   logo_image_file="img/screen/title/logo.png",
   logo_image_x=display.contentCenterX,
-  logo_image_y=370
+  logo_image_y=370,
+  rate_it_button_default_file="img/ui/rate.png",
+  rate_it_button_over_file="img/ui/rate_down.png",
+  rate_it_button_x=display.contentCenterX,
+  rate_it_button_y=900
 }
 return title_scene_config
