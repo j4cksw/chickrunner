9ed98570b60fa102fe0d651c42a8d9228d19c443
Jack Swallow <ninjaadvance@gmail.com>
2014-04-21 17:36:37 +0700

    Add leaderboard button properties to title_scene_config

--- config/title_scene_config.lua
+++ config/title_scene_config.lua
@@ -8,7 +8,11 @@ title_scene_config = {
   logo_image_y=370,
   rate_it_button_default_file="img/ui/rate.png",
   rate_it_button_over_file="img/ui/rate_down.png",
-  rate_it_button_x=display.contentCenterX,
-  rate_it_button_y=890
+  rate_it_button_x=469,
+  rate_it_button_y=890,
+  leaderboard_button_default_file = "img/ui/rank.png",
+  leaderbaord_button_over_file = "img/ui/rank_down.png",
+  leaderboard_button_x = 251,
+  leaderboard_button_y = 890
 }
 return title_scene_config
