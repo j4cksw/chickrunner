acd635a395f19da9f9aca8df550ccbe67aec948b
Jack Swallow <ninjaadvance@gmail.com>
2014-04-23 17:56:53 +0700

    Added creat new switch in create_sound_toggle_switch

--- scene/game/create_sound_toggle_switch.lua
+++ scene/game/create_sound_toggle_switch.lua
@@ -1,2 +1,13 @@
 create_sound_toggle_switch = {}
+
+widget = widget or require("widget")
+
+function create_sound_toggle_switch.evaluate()
+  widget.newSwitch({
+    left = 50,
+    top = 50,
+    style = "checkbox"
+  })
+end
+
 return create_sound_toggle_switch
