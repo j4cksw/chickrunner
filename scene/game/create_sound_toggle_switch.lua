ec5fa555117ca4eb936f7708d289b50595bd96a2
Jack Swallow <ninjaadvance@gmail.com>
2014-04-24 10:26:30 +0700

    fixed fail tests

--- scene/game/create_sound_toggle_switch.lua
+++ scene/game/create_sound_toggle_switch.lua
@@ -1,12 +1,23 @@
 create_sound_toggle_switch = {}
 
 widget = widget or require("widget")
+graphics = graphics or require("graphics")
 
 function create_sound_toggle_switch.evaluate()
+  local sheet = graphics.newImageSheet("img/ui/sound_button.png", {
+    width=69,
+    height=63,
+    numFrames=4
+  })
   widget.newSwitch({
-    left = 50,
-    top = 50,
-    style = "checkbox"
+    x = 680,
+    y = 77,
+    style = "checkbox",
+    sheet = sheet,
+    width=69,
+    height=63,
+    frameOff=1,
+    frameOn=3
   })
 end
 
--- spec/scene/game/create_sound_toggle_switch_spec.lua
+++ spec/scene/game/create_sound_toggle_switch_spec.lua
@@ -1,19 +1,45 @@
 describe("create_sound_toggle_button", function()
   local create_sound_toggle_switch
 
+  local fake_image_sheet = {}
+
   setup(function()
+    graphics = {
+      newImageSheet = function()
+        return fake_image_sheet
+      end
+    }
+    spy.on(graphics, "newImageSheet")
+
     widget = {}
     stub(widget, "newSwitch")
+
     create_sound_toggle_switch = require("scene.game.create_sound_toggle_switch")
+  end)
+
+  it("should create sound toggle image sheet", function()
+    create_sound_toggle_switch.evaluate()
+
+    assert.stub(graphics.newImageSheet).was_called_with(
+      "img/ui/sound_button.png", {
+        width=69,
+        height=63,
+        numFrames=4
+      })
   end)
 
   it("should create sound toggle switch", function()
     create_sound_toggle_switch.evaluate()
 
     assert.stub(widget.newSwitch).was_called_with({
-      left = 50,
-      top = 50,
-      style = "checkbox"
+      x = 680,
+      y = 77,
+      style = "checkbox",
+      sheet = fake_image_sheet,
+      width=69,
+      height=63,
+      frameOff=1,
+      frameOn=3
     })
   end)
 end)
