c4945471309f0e713f648dd725a9ad6cff391aea
Jack Swallow <ninjaadvance@gmail.com>
2014-04-24 16:38:03 +0700

    Implemented mute_all_channel

--- scene/audio/mute_all_channel.lua
+++ scene/audio/mute_all_channel.lua
@@ -1,2 +1,9 @@
 mute_all_channel = {}
+
+audio = audio or require("audio")
+
+function mute_all_channel.evaluate()
+  audio.setVolume(0, {channel=1})
+end
+
 return mute_all_channel
--- spec/scene/audio/mute_all_channel_spec.lua
+++ spec/scene/audio/mute_all_channel_spec.lua
@@ -2,8 +2,15 @@ describe("mute_all_channel", function()
   local mute_all_channel
   
   setup(function()
+    audio = {}
+    stub(audio, "setVolume")
+    
     mute_all_channel = require("scene.audio.mute_all_channel")
   end)
   
-  it("should set volume of all channel to 0")
+  it("should set volume of all channel to 0", function()
+    mute_all_channel.evaluate()
+    
+    assert.stub(audio.setVolume).was_called_with(0, {channel=1})
+  end)
 end)
