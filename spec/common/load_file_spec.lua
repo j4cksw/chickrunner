292aad1dd363cd9e75c753dd1f107b8720231c39
Jack Swallow <ninjaadvance@gmail.com>
2014-04-25 11:03:27 +0700

    Add file file_loader.lua

---
+++ spec/common/load_file_spec.lua
@@ -0,0 +1,88 @@
+describe("load_file", function()
+  local file_loader
+  
+  local fake_content = "..."
+  local fake_game_stat_path = "path/to/game_stat"
+  
+  local fake_game_stat_file = {
+    read = function ( ... )
+      return fake_content
+    end
+  }
+  local expectedResult = {}
+  json = {
+    decode = function ( ... )
+      return expectedResult
+    end
+  }
+  
+  setup(function()
+    system = {
+      pathForFile = function ( ... )
+        return fake_game_stat_path
+      end,
+      DocumentsDirectory = "fakeDocumentDir"
+    }
+    spy.on(system, "pathForFile")
+    
+    io.open = function ( ... )
+      return fake_game_stat_file
+    end
+    spy.on(io, "open")
+    stub(io, "close")
+    
+    spy.on(fake_game_stat_file, "read")
+    
+    spy.on(json, "decode")
+    
+    file_loader = require("common.file_loader")
+  end)
+  
+  it("Acquire path of highscore file", function ( ... )
+    file_loader.load("game_stat")
+    
+    assert.stub(system.pathForFile).was_called_with("game_stat", system.DocumentsDirectory)
+  end)
+  
+  it("Open file", function ( ... )
+    -- when
+    file_loader.load("game_stat")
+    -- then
+    assert.stub(io.open).was_called_with(fake_game_stat_path, "r")
+  end)
+  
+  it("If file availables then read", function ( ... )
+    -- when
+    file_loader.load("game_stat")
+    -- then
+    assert.stub(fake_game_stat_file.read).was_called_with(fake_game_stat_file, "*a")
+  end)
+  
+  it("If file available decode after read", function ( ... )
+    -- when
+    file_loader.load("game_stat")
+    -- then
+    assert.stub(json.decode).was_called_with(fake_content)
+  end)
+  
+  it("If file available close file after decoded", function ( ... )
+    -- when
+    file_loader.load("game_stat")
+    -- then
+    assert.stub(io.close).was_called_with(fake_game_stat_file)
+  end)
+  
+  it("If file available return game_stat table", function ( ... )
+    -- when
+    local result = file_loader.load("game_stat")
+    -- then
+    assert.are.equal(expectedResult, result)
+  end)
+  
+  it("Error if no highscore file available", function ( ... )
+    -- given
+    fake_game_stat_file = nil
+    -- when then
+    assert.has_error(file_loader.load, "Game stat file not found")
+  end)
+end)
---
+++ spec/common/load_options_spec.lua
@@ -0,0 +1,7 @@
+describe("load_options", function()
+  local load_options
+  
+  setup(function()
+    load_options = require("common.load_options")
+  end)
+end)
