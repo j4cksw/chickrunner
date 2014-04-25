f8613b1afa81290d9a55b52c2b72f37d08f82261
Jack Swallow <ninjaadvance@gmail.com>
2014-04-25 13:43:14 +0700

    fixed fail test in file_loader_spec

--- common/file_loader.lua
+++ common/file_loader.lua
@@ -1,6 +1,18 @@
 file_loader = {}
 
-function file_loader.evaluate()
+function file_loader.load(filename)
+  local path = system.pathForFile(filename, system.DocumentsDirectory)
+  local file = io.open(path, "r")
+  
+  if file then
+    local content = file:read("*a")
+    local result = json.decode(content)
+    io.close(file)
+    return result
+  else
+    print("Game stat file not found")
+    error("Game stat file not found")  
+  end
 end
 
 return file_loader
--- spec/common/file_loader_spec.lua
+++ spec/common/file_loader_spec.lua
@@ -45,44 +45,38 @@ describe("load_file", function()
   end)
   
   it("Open file", function ( ... )
-    -- when
     file_loader.load("game_stat")
-    -- then
+    
     assert.stub(io.open).was_called_with(fake_game_stat_path, "r")
   end)
   
   it("If file availables then read", function ( ... )
-    -- when
     file_loader.load("game_stat")
-    -- then
+    
     assert.stub(fake_game_stat_file.read).was_called_with(fake_game_stat_file, "*a")
   end)
   
   it("If file available decode after read", function ( ... )
-    -- when
     file_loader.load("game_stat")
-    -- then
+    
     assert.stub(json.decode).was_called_with(fake_content)
   end)
   
   it("If file available close file after decoded", function ( ... )
-    -- when
     file_loader.load("game_stat")
-    -- then
+    
     assert.stub(io.close).was_called_with(fake_game_stat_file)
   end)
   
   it("If file available return game_stat table", function ( ... )
-    -- when
     local result = file_loader.load("game_stat")
-    -- then
-    assert.are.equal(expectedResult, result)
+    
+    assert.are.same(expectedResult, result)
   end)
   
   it("Error if no highscore file available", function ( ... )
-    -- given
     fake_game_stat_file = nil
-    -- when then
+    
     assert.has_error(file_loader.load, "Game stat file not found")
   end)
 end)
