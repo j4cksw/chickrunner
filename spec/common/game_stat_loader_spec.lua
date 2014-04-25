e2ebb7a5e8c23afd834772920c1b11f519dcf779
Jack Swallow <ninjaadvance@gmail.com>
2014-04-25 13:51:11 +0700

    Add game_stat_loader

---
+++ spec/common/game_stat_loader_spec.lua
@@ -0,0 +1,9 @@
+describe("game_stat_loader", function()
+  local game_stat_loader
+  
+  setup(function()
+    game_stat_loader = require("common.game_stat_loader")
+  end)
+  
+  it("should load content from stat file")
+end)
