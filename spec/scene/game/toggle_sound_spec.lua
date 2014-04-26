7bdc9611773c5c547cbe6c88b3306f5e1510d2fd
Jack Swallow <ninjaadvance@gmail.com>
2014-04-26 23:02:52 +0700

    Add module toggle_sound

---
+++ spec/scene/game/toggle_sound_spec.lua
@@ -0,0 +1,15 @@
+describe("toggle_sound", function()
+  local toggle_sound
+  
+  setup(function()
+    toggle_sound = require("scene.game.toggle_sound")
+  end) 
+  
+  it("should mute audio when swith state is true")
+  
+  it("should unmute audio when switch state is false")
+  
+  it("should save options when switch state is true")
+  
+  it("should save options when switch state is false")
+end)
