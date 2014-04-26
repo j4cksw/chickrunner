539864abfccbae39c6e9e5ff7cc05e27397d4251
Jack Swallow <ninjaadvance@gmail.com>
2014-04-26 23:26:45 +0700

    Add negative test cases to toggle_sound

--- spec/scene/game/toggle_sound_spec.lua
+++ spec/scene/game/toggle_sound_spec.lua
@@ -1,50 +1,74 @@
 describe("toggle_sound", function()
   local toggle_sound
-  
+
   setup(function()
+    toggle_sound = require("scene.game.toggle_sound")
+  end)
+
+  before_each(function()
     unmute_all_channel = {}
     stub(unmute_all_channel, "evaluate")
-    
+
     mute_all_channel = {}
     stub(mute_all_channel, "evaluate")
-    
+
     save_options = {}
     stub(save_options, "evaluate")
-    
-    toggle_sound = require("scene.game.toggle_sound")
-  end) 
-  
+  end)
+
   it("should unmute audio when swith state is true", function()
     toggle_sound.evaluate(true)
-    
+
     assert.stub(unmute_all_channel.evaluate).was_called()
   end)
-  
+
   it("should not call mute audio when switch state is true", function()
     toggle_sound.evaluate(true)
-    
+
     assert.stub(mute_all_channel.evaluate).was_not_called()
   end)
-  
+
   it("should set current state to options table", function()
     options = {
       sound_initial_state=false
     }
-    
+
     toggle_sound.evaluate(true)
-    
+
     assert.are.equal(options.sound_initial_state, true)
   end)
-  
+
   it("should save options when switch state is true", function()
     toggle_sound.evaluate(true)
-    
+
     assert.stub(save_options.evaluate).was_called()
   end)
-  
-  it("should unmute audio when switch state is false")
-  
-  
-  
-  it("should save options when switch state is false")
+
+  it("should not unmute audio when swith state is false", function()
+    toggle_sound.evaluate(false)
+
+    assert.stub(unmute_all_channel.evaluate).was_not_called()
+  end)
+
+  it("should call mute audio when switch state is false", function()
+    toggle_sound.evaluate(false)
+
+    assert.stub(mute_all_channel.evaluate).was_called()
+  end)
+
+  it("should set current state to options table", function()
+    options = {
+      sound_initial_state=true
+    }
+
+    toggle_sound.evaluate(false)
+
+    assert.are.equal(options.sound_initial_state, false)
+  end)
+
+  it("should save options when switch state is false", function()
+    toggle_sound.evaluate(false)
+
+    assert.stub(save_options.evaluate).was_called()
+  end)
 end)
