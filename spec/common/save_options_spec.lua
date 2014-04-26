433ade25a75718430257c9756f58c31515ccfb58
Jack Swallow <ninjaadvance@gmail.com>
2014-04-26 22:53:22 +0700

    Add module save_options

---
+++ spec/common/save_options_spec.lua
@@ -0,0 +1,9 @@
+describe("save_options", function()
+  local save_options
+  
+  setup(function()
+    save_options = require("common.save_options")
+  end)
+  
+  it("should save options table to file")
+end)
