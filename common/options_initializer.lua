6e0df5e7d712d7a9fa726cb741fa346c5e285e71
Jack Swallow <ninjaadvance@gmail.com>
2014-04-26 17:20:01 +0700

    Set loaded options to global 'options'

--- common/options_initializer.lua
+++ common/options_initializer.lua
@@ -4,6 +4,9 @@ file_loader = file_loader or require("common.file_loader")
 
 function options_initializer.initialize()
   local call_result, value = pcall(file_loader.load, "options")
+  if call_result then
+    options = value
+  end
 end
 
 return options_initializer
--- spec/common/options_initializer_spec.lua
+++ spec/common/options_initializer_spec.lua
@@ -1,10 +1,13 @@
 describe("options_initializer", function()
   local options_initializer 
   
+  local stored_options = {
+    is_sound_enabled = false
+  }
   setup(function()
     file_loader = {
       load = function ( ... )
-        return stored_game_stat
+        return stored_options
       end
     }
     spy.on(file_loader, "load")
@@ -18,12 +21,12 @@ describe("options_initializer", function()
     assert.stub(file_loader.load).was_called_with("options")
   end)
 
-  --[[it("should replace highScore table with data in the file if file available", function ( ... )
-    initialize_game_stat.evaluate()
+  it("should replace highScore table with data in the file if file available", function ( ... )
+    options_initializer.initialize()
     
-    assert.are.same(stored_game_stat, game_stat)
+    assert.are.same(stored_options, options)
   end)
-  
+  --[[
   it("should return a default game_stat table when file loading error", function()
     mock_file_loading_error()
 
