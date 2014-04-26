48479e5a22a73e046f70e5d4afd71227b61ebe88
Jack Swallow <ninjaadvance@gmail.com>
2014-04-26 22:54:57 +0700

    Add test case to save_options_spec

--- spec/common/save_options_spec.lua
+++ spec/common/save_options_spec.lua
@@ -1,9 +1,18 @@
 describe("save_options", function()
   local save_options
-  
+
   setup(function()
+    options = {}
+
+    file_saver = {}
+    stub(file_saver, "save")
+    
     save_options = require("common.save_options")
   end)
-  
-  it("should save options table to file")
+
+  it("should save options table to file", function()
+    save_options.evaluate()
+
+    assert.stub(file_saver.save).was_called_with("options", options)
+  end)
 end)
